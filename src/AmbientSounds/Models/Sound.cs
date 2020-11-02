7d5fa1ec07876c56264921daf04e14dbd10ee7bb
Sergio Pedri <sergio0694@live.com>
2020-11-02 16:29:54 +0100

    Switched Sound model to a record

--- src/AmbientSounds/Models/Sound.cs
+++ src/AmbientSounds/Models/Sound.cs
@@ -3,37 +3,15 @@
     /// <summary>
     /// A sound object.
     /// </summary>
-    public class Sound
-    {
-        /// <summary>
-        /// Unique Id for this sound.
-        /// </summary>
-        /// <remarks>
-        /// Generally used to retrieve
-        /// translations for this sound's
-        /// name.
-        /// </remarks>
-        public string Id { get; set; }
-
-        /// <summary>
-        /// Full ms-appx image path.
-        /// </summary>
-        public string ImagePath { get; set; }
-        
-        /// <summary>
-        /// Name of sound.
-        /// </summary>
-        public string Name { get; set; }
-
-        /// <summary>
-        /// Full ms-appx sound file path.
-        /// </summary>
-        public string FilePath { get; set; }
-
-        /// <summary>
-        /// Information about where the sound is from
-        /// and what is its license.
-        /// </summary>
-        public string Attribution { get; set; }
-    }
+    /// <param name="Id">Unique Id for this sound (generally used to retrieve translations for this sound's name).</param>
+    /// <param name="ImagePath">The full path for the associated image for this sound.</param>
+    /// <param name="Name">The name of the sound.</param>
+    /// <param name="FilePath">The full path for the sound file.</param>
+    /// <param name="Attribution">Information about where the sound is from and what is its license.</param>
+    public sealed record Sound(
+        string Id,
+        string ImagePath,
+        string Name,
+        string FilePath,
+        string Attribution);
 }
