e355dc6be2f841aba0c4c3c1edef0e265e0e8cfb
Daniel Paulino <dpaulino@users.noreply.github.com>
2020-11-01 20:33:07 -0800

    Moved logo to separate control

---
+++ AmbientSounds/Controls/LogoControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Numerics;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Hosting;
+using Windows.UI.Xaml.Input;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace AmbientSounds.Controls
+{
+    public sealed partial class LogoControl : UserControl
+    {
+        public LogoControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ImageTapped(object sender, TappedRoutedEventArgs e)
+        {
+            FlyoutBase.ShowAttachedFlyout(sender as FrameworkElement);
+        }
+
+        private void ScaleUp(object sender, PointerRoutedEventArgs e)
+        {
+            // Source for the scaling: https://github.com/windows-toolkit/WindowsCommunityToolkit/blob/master/Microsoft.Toolkit.Uwp.SampleApp/SamplePages/Implicit%20Animations/ImplicitAnimationsPage.xaml.cs
+            // Search for "Scale Element".
+            var element = sender as UIElement;
+            var visual = ElementCompositionPreview.GetElementVisual(element);
+            visual.Scale = new Vector3(1.3f, 1.3f, 1);
+        }
+
+        private void ScaleNormal(object sender, PointerRoutedEventArgs e)
+        {
+            var element = sender as UIElement;
+            var visual = ElementCompositionPreview.GetElementVisual(element);
+            visual.Scale = new Vector3(1, 1, 1);
+        }
+    }
+}
--- AmbientSounds/Views/MainPage.xaml.cs
+++ AmbientSounds/Views/MainPage.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using AmbientSounds.ViewModels;
 using Autofac;
 using System.Numerics;
-using Windows.UI.Composition;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Hosting;
