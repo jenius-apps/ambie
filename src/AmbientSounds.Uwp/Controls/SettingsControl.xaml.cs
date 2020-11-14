09750ef1a9cd8dbc7e4e42ac0a9a88a6a6acff03
Daniel Paulino <dpaulino@users.noreply.github.com>
2020-11-14 15:11:17 -0800

    Added settings user control

---
+++ src/AmbientSounds.Uwp/Controls/SettingsControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using AmbientSounds.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace AmbientSounds.Controls
+{
+    public sealed partial class SettingsControl : UserControl
+    {
+        public SettingsControl()
+        {
+            this.InitializeComponent();
+            this.DataContext = App.Services.GetRequiredService<SettingsViewModel>();
+        }
+
+        public SettingsViewModel ViewModel => (SettingsViewModel)this.DataContext;
+    }
+}
