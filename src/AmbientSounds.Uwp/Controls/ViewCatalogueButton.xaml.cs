d6d63d75b8ff59458418a9f1920d2b41d5175163
Daniel Paulino <dpaulino@users.noreply.github.com>
2020-12-03 23:07:56 -0800

    Added view catalogue button

---
+++ src/AmbientSounds.Uwp/Controls/ViewCatalogueButton.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace AmbientSounds.Controls
+{
+    public sealed partial class ViewCatalogueButton : UserControl
+    {
+        public ViewCatalogueButton()
+        {
+            this.InitializeComponent();
+        }
+
+        private void NavigateToCatalogue()
+        {
+            App.AppFrame.Navigate(
+                typeof(Views.CataloguePage), 
+                null, 
+                new SlideNavigationTransitionInfo() { Effect = SlideNavigationTransitionEffect.FromRight });
+        }
+    }
+}
