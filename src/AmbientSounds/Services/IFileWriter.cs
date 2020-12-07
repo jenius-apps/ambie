5c8a9acdd19d96234f115db64b53ba9ea65bc873
Daniel Paulino <dpaulino@users.noreply.github.com>
2020-12-06 21:35:24 -0800

    Added file writer

---
+++ src/AmbientSounds/Services/IFileWriter.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace AmbientSounds.Services
+{
+    /// <summary>
+    /// Writes ambie data to local directory.
+    /// </summary>
+    public interface IFileWriter
+    {
+        /// <summary>
+        /// Writes sound to local directory.
+        /// </summary>
+        /// <param name="stream">The stream of the sound to write.</param>
+        /// <param name="nameWithExt">The name of the file with extension. E.g. Wind.mp3.</param>
+        /// <returns>The path of the saved file.</returns>
+        Task<string?> WriteSoundAsync(Stream stream, string nameWithExt);
+    }
+}
