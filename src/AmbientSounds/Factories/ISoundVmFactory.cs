84d863058f8c8fa3902e11696d575286a3806229
Daniel Paulino <dpaulino@users.noreply.github.com>
2020-12-10 00:05:05 -0800

    Added sound vm factories

---
+++ src/AmbientSounds/Factories/ISoundVmFactory.cs
@@ -0,0 +1,18 @@
+﻿using AmbientSounds.Models;
+using AmbientSounds.ViewModels;
+
+namespace AmbientSounds.Factories
+{
+    /// <summary>
+    /// Interface for creating sound viewmodels.
+    /// </summary>
+    public interface ISoundVmFactory
+    {
+        /// <summary>
+        /// Creates a new online sound viewmodel.
+        /// </summary>
+        /// <param name="s">The sound to associate with the viewmodel.</param>
+        /// <returns>An online sound viewmodel.</returns>
+        OnlineSoundViewModel GetOnlineSoundVm(Sound s);
+    }
+}
---
+++ src/AmbientSounds/Factories/SoundVmFactory.cs
@@ -0,0 +1,34 @@
+﻿using AmbientSounds.Models;
+using AmbientSounds.Services;
+using AmbientSounds.ViewModels;
+using Microsoft.Toolkit.Diagnostics;
+
+namespace AmbientSounds.Factories
+{
+    /// <summary>
+    /// Creates sound viewmodels.
+    /// </summary>
+    public class SoundVmFactory : ISoundVmFactory
+    {
+        private readonly IDownloadManager _downloadManager;
+        private readonly ISoundDataProvider _soundDataProvider;
+
+        public SoundVmFactory(
+            IDownloadManager downloadManager,
+            ISoundDataProvider soundDataProvider)
+        {
+            Guard.IsNotNull(downloadManager, nameof(downloadManager));
+            Guard.IsNotNull(soundDataProvider, nameof(soundDataProvider));
+
+            _downloadManager = downloadManager;
+            _soundDataProvider = soundDataProvider;
+        }
+
+        /// <inheritdoc/>
+        public OnlineSoundViewModel GetOnlineSoundVm(Sound s)
+        {
+            Guard.IsNotNull(s, nameof(s));
+            return new OnlineSoundViewModel(s, _downloadManager, _soundDataProvider);
+        }
+    }
+}
