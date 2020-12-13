7482f1795e379ebd5ed85fd377706e3de57d3d69
Daniel Paulino <dpaulino@users.noreply.github.com>
2020-12-12 20:52:41 -0800

    Added GetSoundVm

--- src/AmbientSounds/Factories/ISoundVmFactory.cs
+++ src/AmbientSounds/Factories/ISoundVmFactory.cs
@@ -14,5 +14,13 @@ namespace AmbientSounds.Factories
         /// <param name="s">The sound to associate with the viewmodel.</param>
         /// <returns>An online sound viewmodel.</returns>
         OnlineSoundViewModel GetOnlineSoundVm(Sound s);
+
+        /// <summary>
+        /// Creates new sound viewmodel.
+        /// </summary>
+        /// <param name="s">The sound to associate with the viewmodel.</param>
+        /// <param name="index">The index of the sound in the list.</param>
+        /// <returns>A sound viewmodel.</returns>
+        SoundViewModel GetSoundVm(Sound s, int index);
     }
 }
--- src/AmbientSounds/Factories/SoundVmFactory.cs
+++ src/AmbientSounds/Factories/SoundVmFactory.cs
@@ -12,16 +12,20 @@ namespace AmbientSounds.Factories
     {
         private readonly IDownloadManager _downloadManager;
         private readonly ISoundDataProvider _soundDataProvider;
+        private readonly IMediaPlayerService _player;
 
         public SoundVmFactory(
             IDownloadManager downloadManager,
+            IMediaPlayerService player,
             ISoundDataProvider soundDataProvider)
         {
             Guard.IsNotNull(downloadManager, nameof(downloadManager));
             Guard.IsNotNull(soundDataProvider, nameof(soundDataProvider));
+            Guard.IsNotNull(player, nameof(player));
 
             _downloadManager = downloadManager;
             _soundDataProvider = soundDataProvider;
+            _player = player;
         }
 
         /// <inheritdoc/>
@@ -29,6 +33,14 @@ namespace AmbientSounds.Factories
         {
             Guard.IsNotNull(s, nameof(s));
             return new OnlineSoundViewModel(s, _downloadManager, _soundDataProvider);
+        }
+
+        /// <inheritdoc/>
+        public SoundViewModel GetSoundVm(Sound s, int index)
+        {
+            Guard.IsNotNull(s, nameof(s));
+            Guard.IsGreaterThan(index, -1, nameof(index));
+            return new SoundViewModel(s, _player, index, _soundDataProvider);
         }
     }
 }
