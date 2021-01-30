aa7c8e435d9f8308926c256d923ba1cb23cbd3ca
Daniel Paulino <dpaulino@users.noreply.github.com>
2021-01-30 11:28:48 -0800

    Added mix media player service stub

---
+++ src/AmbientSounds/Services/IMixMediaPlayerService.cs
@@ -0,0 +1,16 @@
+﻿using AmbientSounds.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AmbientSounds.Services
+{
+    public interface IMixMediaPlayerService
+    {
+        event EventHandler<Sound> SoundAdded;
+
+        event EventHandler<Sound> SoundRemoved;
+
+        void AddSound(Sound s);
+    }
+}
