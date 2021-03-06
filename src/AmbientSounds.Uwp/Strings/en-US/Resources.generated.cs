7f27ed908383f851a556230a1596dacb02f38922
Daniel Paulino <dpaulino@users.noreply.github.com>
2021-03-05 22:56:48 -0800

    Adding resw plus

---
+++ src/AmbientSounds.Uwp/Strings/en-US/Resources.generated.cs
@@ -0,0 +1,458 @@
+// File generated automatically by ReswPlus. https://github.com/DotNetPlus/ReswPlus
+using System;
+using Windows.ApplicationModel.Resources;
+using Windows.UI.Xaml.Markup;
+using Windows.UI.Xaml.Data;
+
+namespace AmbientSounds.Strings{
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("DotNetPlus.ReswPlus", "2.1.3")]
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    public static class Resources {
+        private static ResourceLoader _resourceLoader;
+        static Resources()
+        {
+            _resourceLoader = ResourceLoader.GetForViewIndependentUse("Resources");
+        }
+
+        #region CloseText
+        /// <summary>
+        ///   Looks up a localized string similar to: Close
+        /// </summary>
+        public static string CloseText
+        {
+            get
+            {
+                return _resourceLoader.GetString("CloseText");
+            }
+        }
+        #endregion
+
+        #region PlayerPauseText
+        /// <summary>
+        ///   Looks up a localized string similar to: Pause
+        /// </summary>
+        public static string PlayerPauseText
+        {
+            get
+            {
+                return _resourceLoader.GetString("PlayerPauseText");
+            }
+        }
+        #endregion
+
+        #region PlayerPlayText
+        /// <summary>
+        ///   Looks up a localized string similar to: Play
+        /// </summary>
+        public static string PlayerPlayText
+        {
+            get
+            {
+                return _resourceLoader.GetString("PlayerPlayText");
+            }
+        }
+        #endregion
+
+        #region ReadyToPlayText
+        /// <summary>
+        ///   Looks up a localized string similar to: Ready to play
+        /// </summary>
+        public static string ReadyToPlayText
+        {
+            get
+            {
+                return _resourceLoader.GetString("ReadyToPlayText");
+            }
+        }
+        #endregion
+
+        #region SettingsText
+        /// <summary>
+        ///   Looks up a localized string similar to: Settings
+        /// </summary>
+        public static string SettingsText
+        {
+            get
+            {
+                return _resourceLoader.GetString("SettingsText");
+            }
+        }
+        #endregion
+
+        #region Soundbeach
+        /// <summary>
+        ///   Looks up a localized string similar to: Beach
+        /// </summary>
+        public static string Soundbeach
+        {
+            get
+            {
+                return _resourceLoader.GetString("Soundbeach");
+            }
+        }
+        #endregion
+
+        #region Soundbirds
+        /// <summary>
+        ///   Looks up a localized string similar to: Birds
+        /// </summary>
+        public static string Soundbirds
+        {
+            get
+            {
+                return _resourceLoader.GetString("Soundbirds");
+            }
+        }
+        #endregion
+
+        #region Soundcitystreet
+        /// <summary>
+        ///   Looks up a localized string similar to: City street
+        /// </summary>
+        public static string Soundcitystreet
+        {
+            get
+            {
+                return _resourceLoader.GetString("Soundcitystreet");
+            }
+        }
+        #endregion
+
+        #region Soundcoffeeshop
+        /// <summary>
+        ///   Looks up a localized string similar to: Coffee shop
+        /// </summary>
+        public static string Soundcoffeeshop
+        {
+            get
+            {
+                return _resourceLoader.GetString("Soundcoffeeshop");
+            }
+        }
+        #endregion
+
+        #region Soundcreek
+        /// <summary>
+        ///   Looks up a localized string similar to: Creek
+        /// </summary>
+        public static string Soundcreek
+        {
+            get
+            {
+                return _resourceLoader.GetString("Soundcreek");
+            }
+        }
+        #endregion
+
+        #region Soundfireplace
+        /// <summary>
+        ///   Looks up a localized string similar to: Fireplace
+        /// </summary>
+        public static string Soundfireplace
+        {
+            get
+            {
+                return _resourceLoader.GetString("Soundfireplace");
+            }
+        }
+        #endregion
+
+        #region Soundmore
+        /// <summary>
+        ///   Looks up a localized string similar to: More coming soon
+        /// </summary>
+        public static string Soundmore
+        {
+            get
+            {
+                return _resourceLoader.GetString("Soundmore");
+            }
+        }
+        #endregion
+
+        #region Soundrain
+        /// <summary>
+        ///   Looks up a localized string similar to: Rain
+        /// </summary>
+        public static string Soundrain
+        {
+            get
+            {
+                return _resourceLoader.GetString("Soundrain");
+            }
+        }
+        #endregion
+
+        #region Soundrainforest
+        /// <summary>
+        ///   Looks up a localized string similar to: Rainforest
+        /// </summary>
+        public static string Soundrainforest
+        {
+            get
+            {
+                return _resourceLoader.GetString("Soundrainforest");
+            }
+        }
+        #endregion
+
+        #region Soundunderwater
+        /// <summary>
+        ///   Looks up a localized string similar to: Underwater
+        /// </summary>
+        public static string Soundunderwater
+        {
+            get
+            {
+                return _resourceLoader.GetString("Soundunderwater");
+            }
+        }
+        #endregion
+
+        #region Soundwaterfall
+        /// <summary>
+        ///   Looks up a localized string similar to: Waterfall
+        /// </summary>
+        public static string Soundwaterfall
+        {
+            get
+            {
+                return _resourceLoader.GetString("Soundwaterfall");
+            }
+        }
+        #endregion
+
+        #region Soundwhitenoise
+        /// <summary>
+        ///   Looks up a localized string similar to: White noise
+        /// </summary>
+        public static string Soundwhitenoise
+        {
+            get
+            {
+                return _resourceLoader.GetString("Soundwhitenoise");
+            }
+        }
+        #endregion
+
+        #region SoundUnplayable
+        /// <summary>
+        ///   Looks up a localized string similar to: Cannot play sound
+        /// </summary>
+        public static string SoundUnplayable
+        {
+            get
+            {
+                return _resourceLoader.GetString("SoundUnplayable");
+            }
+        }
+        #endregion
+
+        #region Soundvacuum
+        /// <summary>
+        ///   Looks up a localized string similar to: Vacuum
+        /// </summary>
+        public static string Soundvacuum
+        {
+            get
+            {
+                return _resourceLoader.GetString("Soundvacuum");
+            }
+        }
+        #endregion
+
+        #region Soundthunder
+        /// <summary>
+        ///   Looks up a localized string similar to: Thunder
+        /// </summary>
+        public static string Soundthunder
+        {
+            get
+            {
+                return _resourceLoader.GetString("Soundthunder");
+            }
+        }
+        #endregion
+
+        #region Soundwind
+        /// <summary>
+        ///   Looks up a localized string similar to: Wind
+        /// </summary>
+        public static string Soundwind
+        {
+            get
+            {
+                return _resourceLoader.GetString("Soundwind");
+            }
+        }
+        #endregion
+
+        #region AlreadyDownloaded
+        /// <summary>
+        ///   Looks up a localized string similar to: Already downloaded
+        /// </summary>
+        public static string AlreadyDownloaded
+        {
+            get
+            {
+                return _resourceLoader.GetString("AlreadyDownloaded");
+            }
+        }
+        #endregion
+
+        #region CanDownload
+        /// <summary>
+        ///   Looks up a localized string similar to: Can be downloaded
+        /// </summary>
+        public static string CanDownload
+        {
+            get
+            {
+                return _resourceLoader.GetString("CanDownload");
+            }
+        }
+        #endregion
+
+        #region Paused
+        /// <summary>
+        ///   Looks up a localized string similar to: Paused
+        /// </summary>
+        public static string Paused
+        {
+            get
+            {
+                return _resourceLoader.GetString("Paused");
+            }
+        }
+        #endregion
+
+        #region Playing
+        /// <summary>
+        ///   Looks up a localized string similar to: Playing
+        /// </summary>
+        public static string Playing
+        {
+            get
+            {
+                return _resourceLoader.GetString("Playing");
+            }
+        }
+        #endregion
+
+        #region CancelText
+        /// <summary>
+        ///   Looks up a localized string similar to: Cancel
+        /// </summary>
+        public static string CancelText
+        {
+            get
+            {
+                return _resourceLoader.GetString("CancelText");
+            }
+        }
+        #endregion
+
+        #region RenameText
+        /// <summary>
+        ///   Looks up a localized string similar to: Rename
+        /// </summary>
+        public static string RenameText
+        {
+            get
+            {
+                return _resourceLoader.GetString("RenameText");
+            }
+        }
+        #endregion
+
+        #region MissingSoundsTitle
+        /// <summary>
+        ///   Looks up a localized string similar to: Missing sounds
+        /// </summary>
+        public static string MissingSoundsTitle
+        {
+            get
+            {
+                return _resourceLoader.GetString("MissingSoundsTitle");
+            }
+        }
+        #endregion
+
+        #region SignInDescriptionText
+        /// <summary>
+        ///   Looks up a localized string similar to: This will enable sound gallery sync across your devices.
+        /// </summary>
+        public static string SignInDescriptionText
+        {
+            get
+            {
+                return _resourceLoader.GetString("SignInDescriptionText");
+            }
+        }
+        #endregion
+    }
+
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("DotNetPlus.ReswPlus", "2.1.3")]
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    [MarkupExtensionReturnType(ReturnType = typeof(string))]
+    public class ResourcesExtension: MarkupExtension
+    {
+        public enum KeyEnum
+        {
+            __Undefined = 0,
+            CloseText,
+            PlayerPauseText,
+            PlayerPlayText,
+            ReadyToPlayText,
+            SettingsText,
+            Soundbeach,
+            Soundbirds,
+            Soundcitystreet,
+            Soundcoffeeshop,
+            Soundcreek,
+            Soundfireplace,
+            Soundmore,
+            Soundrain,
+            Soundrainforest,
+            Soundunderwater,
+            Soundwaterfall,
+            Soundwhitenoise,
+            SoundUnplayable,
+            Soundvacuum,
+            Soundthunder,
+            Soundwind,
+            AlreadyDownloaded,
+            CanDownload,
+            Paused,
+            Playing,
+            CancelText,
+            RenameText,
+            MissingSoundsTitle,
+            SignInDescriptionText,
+        }
+
+        private static ResourceLoader _resourceLoader;
+        static ResourcesExtension()
+        {
+            _resourceLoader = ResourceLoader.GetForViewIndependentUse("Resources");
+        }
+        public KeyEnum Key { get; set;}
+        public IValueConverter Converter { get; set;}
+        public object ConverterParameter { get; set;}
+        protected override object ProvideValue()
+        {
+            string res;
+            if(Key == KeyEnum.__Undefined)
+            {
+                res = "";
+            }
+            else
+            {
+                res = _resourceLoader.GetString(Key.ToString());
+            }
+            return Converter == null ? res : Converter.Convert(res, typeof(String), ConverterParameter, null);
+        }
+    }
+} //AmbientSounds.Strings
