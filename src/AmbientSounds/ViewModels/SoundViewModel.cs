3ded2ebfcd0297273b9cfd75c1ae285a886f7e45
Daniel Paulino <dpaulino@users.noreply.github.com>
2021-03-28 00:28:40 -0700

    Fixed bug with images being displayed

--- src/AmbientSounds/ViewModels/SoundViewModel.cs
+++ src/AmbientSounds/ViewModels/SoundViewModel.cs
@@ -79,13 +79,13 @@ namespace AmbientSounds.ViewModels
 
         public bool IsNotMix => !IsMix;
 
-        public bool HasSecondImage => IsMix && _sound.ImagePaths.Length >= 2;
+        public bool HasSecondImage => IsMix && _sound.ImagePaths.Length == 2;
 
-        public string? SecondImagePath => HasSecondImage ? _sound.ImagePaths[1] : "http://localhost:8000";
+        public string? SecondImagePath => _sound.ImagePaths.Length >= 2 ? _sound.ImagePaths[1] : "http://localhost:8000";
 
-        public bool HasThirdImage => IsMix && _sound.ImagePaths.Length >= 3;
+        public bool HasThirdImage => IsMix && _sound.ImagePaths.Length == 3;
 
-        public string? ThirdImagePath => HasThirdImage ? _sound.ImagePaths[2] : "http://localhost:8000";
+        public string? ThirdImagePath => _sound.ImagePaths.Length >= 3 ? _sound.ImagePaths[2] : "http://localhost:8000";
 
         /// <summary>
         /// The path for the image to display for the current sound.
