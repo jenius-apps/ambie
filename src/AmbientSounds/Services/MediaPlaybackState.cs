5cea17bb0fd1e530ff5bc2f5aeb463231f98cb98
Daniel Paulino <dpaulino@users.noreply.github.com>
2021-04-06 21:59:57 -0700

    Code cleanup

---
+++ src/AmbientSounds/Services/MediaPlaybackState.cs
@@ -0,0 +1,28 @@
+﻿namespace AmbientSounds.Services
+{
+    /// <summary>
+    /// An enum representing possible media playback states.
+    /// </summary>
+    public enum MediaPlaybackState
+    {
+        /// <summary>
+        /// A file is currently being opened or buffered.
+        /// </summary>
+        Opening,
+
+        /// <summary>
+        /// A track is currently being played.
+        /// </summary>
+        Playing,
+
+        /// <summary>
+        /// A track is opened but currently paused.
+        /// </summary>
+        Paused,
+
+        /// <summary>
+        /// The media playback is currently stopped.
+        /// </summary>
+        Stopped
+    }
+}
