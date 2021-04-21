e14f74d27c7672eb5effb0014c8d6a156dddb936
Daniel Paulino <dpaulino@users.noreply.github.com>
2021-04-20 20:09:06 -0700

    Added test for formatted duration

--- AmbientSounds.Tests/PlayerTrackerTests.cs
+++ AmbientSounds.Tests/PlayerTrackerTests.cs
@@ -1,13 +1,14 @@
 using System;
 using Xunit;
 using AmbientSounds.Services;
+using Moq;
 
 namespace AmbientSounds.Tests
 {
     public class PlayerTrackerTests
     {
         [Theory]
-        [InlineData(-1, "")] // 30 seconds
+        [InlineData(-1, "")] // invalid data
         [InlineData(30, "<1 min")] // 30 seconds
         [InlineData(180, "<5 min")] // 3 minutes
         [InlineData(480, "<10 min")] // 8 minutes
@@ -22,6 +23,18 @@ namespace AmbientSounds.Tests
             TimeSpan diff = TimeSpan.FromSeconds(diffInSeconds);
             var result = PlayerTelemetryTracker.GetRoundedDiff(diff);
             Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void TrackDurationTest()
+        {
+            var playerMock = new Mock<IMixMediaPlayerService>();
+            var telemetryMock = new Mock<ITelemetry>();
+
+            var tracker = new PlayerTelemetryTracker(playerMock.Object, telemetryMock.Object);
+            string result = tracker.TrackDuration(DateTimeOffset.Now);
+
+            Assert.Equal(string.Empty, result);
         }
     }
 }
