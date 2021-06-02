e27ddd180653c243d3ea93b97c80cf6e62cb688e
Daniel Paulino <dpaulino@users.noreply.github.com>
2021-06-02 00:47:36 -0700

    Adding support for narrow sound grid layout

--- src/AmbientSounds.Uwp/Controls/SoundGridControl.xaml.cs
+++ src/AmbientSounds.Uwp/Controls/SoundGridControl.xaml.cs
@@ -11,6 +11,18 @@ namespace AmbientSounds.Controls
 {
     public sealed partial class SoundGridControl : UserControl
     {
+        public static readonly DependencyProperty ItemTemplateProperty = DependencyProperty.Register(
+            nameof(ItemTemplate),
+            typeof(DataTemplate),
+            typeof(SoundGridControl),
+            null);
+
+        public static readonly DependencyProperty LayoutProperty = DependencyProperty.Register(
+            nameof(Layout),
+            typeof(Microsoft.UI.Xaml.Controls.Layout),
+            typeof(SoundGridControl),
+            null);
+
         public SoundGridControl()
         {
             this.InitializeComponent();
@@ -21,6 +33,12 @@ namespace AmbientSounds.Controls
 
         public SoundListViewModel ViewModel => (SoundListViewModel)this.DataContext;
 
+        public Microsoft.UI.Xaml.Controls.Layout? Layout
+        {
+            get => (Microsoft.UI.Xaml.Controls.Layout?)GetValue(LayoutProperty);
+            set => SetValue(LayoutProperty, value);
+        }
+
         /// <summary>
         /// If true, the compact mode button is visible.
         /// Default is true.
@@ -30,15 +48,5 @@ namespace AmbientSounds.Controls
             get => (DataTemplate?)GetValue(ItemTemplateProperty);
             set => SetValue(ItemTemplateProperty, value);
         }
-
-        /// <summary>
-        /// Dependency property for <see cref="ItemTemplate"/>.
-        /// Default is true.
-        /// </summary>
-        public static readonly DependencyProperty ItemTemplateProperty = DependencyProperty.Register(
-            nameof(ItemTemplate),
-            typeof(bool),
-            typeof(SoundGridControl),
-            null);
     }
 }
