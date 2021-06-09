4c16a93d90daba774750d9bfdaa4feafd966b30d
Daniel Paulino <dpaulino@users.noreply.github.com>
2021-06-09 08:17:08 -0700

    Updated compact page

--- src/AmbientSounds.Uwp/Views/CompactPage.xaml.cs
+++ src/AmbientSounds.Uwp/Views/CompactPage.xaml.cs
@@ -1,13 +1,10 @@
-﻿using AmbientSounds.Animations;
-using AmbientSounds.Constants;
+﻿using AmbientSounds.Constants;
 using AmbientSounds.Services;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using Windows.UI.ViewManagement;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Navigation;
 
 #nullable enable
@@ -19,10 +16,15 @@ namespace AmbientSounds.Views
     /// </summary>
     public sealed partial class CompactPage : Page
     {
+        private readonly IUserSettings _userSettings;
+
         public CompactPage()
         {
             this.InitializeComponent();
+            _userSettings = App.Services.GetRequiredService<IUserSettings>();
         }
+
+        private string BackgroundImagePath => _userSettings.Get<string>(UserSettingsConstants.BackgroundImage);
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
@@ -31,6 +33,17 @@ namespace AmbientSounds.Views
             {
                 { "name", "compact" }
             });
+
+            UpdateBackgroundState();
+        }
+
+        private void UpdateBackgroundState()
+        {
+            bool backgroundImageActive = !string.IsNullOrEmpty(BackgroundImagePath);
+            if (backgroundImageActive)
+            {
+                FindName(nameof(BackgroundImage));
+            }
         }
 
         private async void CloseCompactClicked()
