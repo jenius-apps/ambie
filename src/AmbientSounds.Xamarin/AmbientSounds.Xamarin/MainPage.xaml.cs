0bd8d0442ce5c0bfbc3d8127526b99c5b788369a
Daniel Paulino <dpaulino@users.noreply.github.com>
2021-07-25 15:23:06 -0700

    Initial commit of ios

---
+++ src/AmbientSounds.Xamarin/AmbientSounds.Xamarin/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AmbientSounds.Xamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
