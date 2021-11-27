99a3e859738cc57df93556e3a775be9ffb4878d8
Sergio Pedri <sergio0694@live.com>
2021-11-27 19:25:32 +0100

    Add ComputeSharp test page

---
+++ src/AmbientSounds.Uwp/Views/ComputeSharpTestPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using AmbientSounds.Shaders;
+using ComputeSharp.Uwp;
+using Windows.UI.Xaml.Controls;
+
+namespace AmbientSounds.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ComputeSharpTestPage : Page
+    {
+        public ComputeSharpTestPage()
+        {
+            this.InitializeComponent();
+
+            ShaderPanel.ShaderRunner = new ShaderRunner<ColorfulInfinity>(static time => new ColorfulInfinity((float)time.TotalSeconds / 4f));
+        }
+    }
+}
