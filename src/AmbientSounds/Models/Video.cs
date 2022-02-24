aeda49c9af02de9592b9b35da4109d2384ac8bcc
Daniel Paulino <dpaulino@users.noreply.github.com>
2022-02-24 00:11:27 -0800

    Added video object

---
+++ src/AmbientSounds/Models/Video.cs
@@ -0,0 +1,44 @@
+﻿namespace AmbientSounds.Models
+{
+    /// <summary>
+    /// An object representing a video that can be downloaded
+    /// and played by Ambie.
+    /// </summary>
+    public class Video
+    {
+        /// <summary>
+        /// GUID to uniquely identify the video. Required by database.
+        /// </summary>
+        public string Id { get; set; } = string.Empty;
+
+        /// <summary>
+        /// User-friendly name that can be used by a client.
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// True if the vidoe has been downloaded and is available
+        /// for offline playback.
+        /// </summary>
+        public bool IsDownloaded { get; set; }
+
+        /// <summary>
+        /// The download URL for the video. This can
+        /// be empty, particularly if the video has been downloaded
+        /// already.
+        /// </summary>
+        public string DownloadUrl { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Local path for the video. This can be empty,
+        /// particularly if the video has not been downloaded
+        /// yet.
+        /// </summary>
+        public string FilePath { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Size of video file in MB.
+        /// </summary>
+        public double MegaByteSize { get; set; }
+    }
+}
