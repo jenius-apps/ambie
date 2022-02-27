9ef535134d55b9eb4890d3da2bd5195945aee8a1
Daniel Paulino <dpaulino@users.noreply.github.com>
2022-02-26 18:50:15 -0800

    Added offline video repo

---
+++ src/AmbientSounds/Repositories/IOfflineVideoRepository.cs
@@ -0,0 +1,17 @@
+﻿using AmbientSounds.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmbientSounds.Repositories
+{
+    public interface IOfflineVideoRepository
+    {
+        /// <summary>
+        /// Retrives a list of video metadata from
+        /// an offline source.
+        /// </summary>
+        Task<IReadOnlyList<Video>> GetVideosAsync();
+    }
+}
