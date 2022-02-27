e647377c33083619d4d3c64c8c5484e69f78cd99
Daniel Paulino <dpaulino@users.noreply.github.com>
2022-02-26 18:51:23 -0800

    Added video cache

---
+++ src/AmbientSounds/Cache/IVideoCache.cs
@@ -0,0 +1,23 @@
+﻿using AmbientSounds.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AmbientSounds.Cache
+{
+    public interface IVideoCache
+    {
+        /// <summary>
+        /// Retrieves dictionary of offline video data.
+        /// If cache is empty, new data will be retrieved.
+        /// Otherwise, cache is returned.
+        /// </summary>
+        Task<IReadOnlyDictionary<string, Video>> GetOfflineVideosAsync();
+
+        /// <summary>
+        /// Retrieves dictionary of online video data.
+        /// If cache is empty, new data will be retrieved.
+        /// Otherwise, cache is returned.
+        /// </summary>
+        Task<IReadOnlyList<Video>> GetOnlineVideosAsync();
+    }
+}
