5de1b28ab97ded07c059002eda927367a16f2558
Daniel Paulino <dpaulino@users.noreply.github.com>
2022-04-08 22:13:19 -0700

    Added ring backgrounds

--- src/AmbientSounds/ViewModels/FocusPageViewModel.cs
+++ src/AmbientSounds/ViewModels/FocusPageViewModel.cs
@@ -13,6 +13,11 @@ namespace AmbientSounds.ViewModels
         private int _focusLength;
         private int _restLength;
         private int _repetitions;
+        private int _focusLengthRemaining;
+        private int _restLengthRemaining;
+        private int _repetitionsRemaining;
+        private string _currentTimeRemaining = string.Empty;
+        private string _currentStatus = string.Empty;
 
         public FocusPageViewModel(IFocusService focusService)
         {
@@ -27,6 +32,7 @@ namespace AmbientSounds.ViewModels
             { 
                 SetProperty(ref _focusLength, value);
                 OnPropertyChanged(nameof(TotalTime));
+                FocusLengthRemaining = value;
             }
         }
 
@@ -37,6 +43,7 @@ namespace AmbientSounds.ViewModels
             {
                 SetProperty(ref _restLength, value);
                 OnPropertyChanged(nameof(TotalTime));
+                RestLengthRemaining = value;
             }
         }
 
@@ -47,6 +54,7 @@ namespace AmbientSounds.ViewModels
             {
                 SetProperty(ref _repetitions, value);
                 OnPropertyChanged(nameof(TotalTime));
+                RepetitionsRemaining = value;
             }
         }
 
@@ -56,6 +64,51 @@ namespace AmbientSounds.ViewModels
             {
                 TimeSpan time = _focusService.GetTotalTime(FocusLength, RestLength, Repetitions);
                 return time.ToString(@"hh\:mm");
+            }
+        }
+
+        public int FocusLengthRemaining
+        {
+            get => _focusLengthRemaining;
+            set
+            {
+                SetProperty(ref _focusLengthRemaining, value);
+            }
+        }
+
+        public int RestLengthRemaining
+        {
+            get => _restLengthRemaining;
+            set
+            {
+                SetProperty(ref _restLengthRemaining, value);
+            }
+        }
+
+        public int RepetitionsRemaining
+        {
+            get => _repetitionsRemaining;
+            set
+            {
+                SetProperty(ref _repetitionsRemaining, value);
+            }
+        }
+
+        public string CurrentStatus
+        {
+            get => _currentStatus;
+            set
+            {
+                SetProperty(ref _currentStatus, value);
+            }
+        }
+
+        public string CurrentTimeRemaining
+        {
+            get => _currentTimeRemaining;
+            set
+            {
+                SetProperty(ref _currentTimeRemaining, value);
             }
         }
     }
