a962f14fdb1bacc264934ba2d6f1ca598d48a2ef
Daniel Paulino <dpaulino@users.noreply.github.com>
2022-06-05 10:11:47 -0700

    WIP breathe page

---
+++ src/AmbientSounds.Uwp/Views/BreathePage.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+using Microsoft.Graphics.Canvas.UI;
+using Microsoft.Graphics.Canvas.Effects;
+using Microsoft.Graphics.Canvas;
+using System.Numerics;
+using Windows.UI;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace AmbientSounds.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class BreathePage : Page
+    {
+        GaussianBlurEffect blur;
+
+        public BreathePage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void canvas_CreateResources(CanvasAnimatedControl sender, CanvasCreateResourcesEventArgs args)
+        {
+            CanvasCommandList cl = new CanvasCommandList(sender);
+            using (CanvasDrawingSession clds = cl.CreateDrawingSession())
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    clds.DrawText("Hello, World!", RndPosition(), Color.FromArgb(255, RndByte(), RndByte(), RndByte()));
+                    clds.DrawCircle(RndPosition(), RndRadius(), Color.FromArgb(255, RndByte(), RndByte(), RndByte()));
+                    clds.DrawLine(RndPosition(), RndPosition(), Color.FromArgb(255, RndByte(), RndByte(), RndByte()));
+                }
+            }
+
+            blur = new GaussianBlurEffect()
+            {
+                Source = cl,
+                BlurAmount = 10.0f
+            };
+        }
+
+        private void canvas_DrawAnimated(ICanvasAnimatedControl sender, CanvasAnimatedDrawEventArgs args)
+        {
+            float radius = (float)(1 + Math.Sin(args.Timing.TotalTime.TotalSeconds)) * 10f;
+            blur.BlurAmount = radius;
+            args.DrawingSession.DrawImage(blur);
+        }
+
+        Random rnd = new Random();
+        private Vector2 RndPosition()
+        {
+            double x = rnd.NextDouble() * 500f;
+            double y = rnd.NextDouble() * 500f;
+            return new Vector2((float)x, (float)y);
+        }
+
+        private float RndRadius()
+        {
+            return (float)rnd.NextDouble() * 150f;
+        }
+
+        private byte RndByte()
+        {
+            return (byte)rnd.Next(256);
+        }
+    }
+}
