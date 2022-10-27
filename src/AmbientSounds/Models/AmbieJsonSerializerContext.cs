a15c061307086368d1861c8fe08545dad4939cc3
Sergio Pedri <sergio0694@live.com>
2022-10-27 23:26:38 +0200

    Add comments to JSON context

--- src/AmbientSounds/Models/AmbieJsonSerializerContext.cs
+++ src/AmbientSounds/Models/AmbieJsonSerializerContext.cs
@@ -1,22 +1,38 @@
 ﻿using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
-namespace AmbientSounds.Models
+namespace AmbientSounds.Models;
+
+/// <summary>
+/// The <see cref="JsonSerializerContext"/> with type info for all JSON models used by Ambie.
+/// </summary>
+[JsonSerializable(typeof(FocusHistory))]
+[JsonSerializable(typeof(FocusHistorySummary))]
+[JsonSerializable(typeof(FocusTask[]), GenerationMode = JsonSourceGenerationMode.Metadata)] // Only used to deserialize
+[JsonSerializable(typeof(IEnumerable<FocusTask>))]
+[JsonSerializable(typeof(Video[]), GenerationMode = JsonSourceGenerationMode.Metadata)] // Only used to deserialize
+[JsonSerializable(typeof(IList<Video>))]
+[JsonSerializable(typeof(SyncData))]
+[JsonSerializable(typeof(Sound[]), GenerationMode = JsonSourceGenerationMode.Metadata)] // Only used to deserialize
+[JsonSerializable(typeof(List<Sound>))]
+[JsonSerializable(typeof(RecentFocusSettings[]))]
+public sealed partial class AmbieJsonSerializerContext : JsonSerializerContext
 {
-    /// <summary>
-    /// The <see cref="JsonSerializerContext"/> with type info for all JSON models used by Ambie.
-    /// </summary>
-    [JsonSerializable(typeof(FocusHistory))]
-    [JsonSerializable(typeof(FocusHistorySummary))]
-    [JsonSerializable(typeof(FocusTask[]), GenerationMode = JsonSourceGenerationMode.Metadata)] // Only used to deserialize
-    [JsonSerializable(typeof(IEnumerable<FocusTask>))]
-    [JsonSerializable(typeof(Video[]), GenerationMode = JsonSourceGenerationMode.Metadata)] // Only used to deserialize
-    [JsonSerializable(typeof(IList<Video>))]
-    [JsonSerializable(typeof(SyncData))]
-    [JsonSerializable(typeof(Sound[]), GenerationMode = JsonSourceGenerationMode.Metadata)] // Only used to deserialize
-    [JsonSerializable(typeof(List<Sound>))]
-    [JsonSerializable(typeof(RecentFocusSettings[]))]
-    public sealed partial class AmbieJsonSerializerContext : JsonSerializerContext
-    {
-    }
+    // This type acts as the receiver for the System.Text.Json generator to generate all the JSON serialization
+    // code for the types marked as annotations here. That is, instead of using runtime reflection, the generator
+    // will generate all the necessary serialization code at build time, so that all the code can be trimmed and
+    // also run faster at runtime. See: https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/source-generation.
+    //
+    // There are two types of annotations used here, primarily:
+    //   - Types that are only deserialized are marked with JsonSourceGenerationMode.Metadata. This is only a subset of the
+    //     code that's possible to generate, and it won't include the deserialization code, as that's not needed.
+    //   - Types that are both serialized and deserialized just use the default setting, which include both modes.
+    //
+    // If you add a new type that needs to use JSON serialization, make sure to add an attribute here. Then to serialize:
+    //
+    // Do NOT do this as before: JsonSerializer.Serialize(myModel);
+    //
+    // Do THIS instead:          JsonSerializer.Serialize(myModel, AmbieJsonSerializerContext.Default.MyModel);
+    //
+    // That is, pass the generated JsonTypeInfo<T> property that matches that model type. Same to deserialize.
 }
