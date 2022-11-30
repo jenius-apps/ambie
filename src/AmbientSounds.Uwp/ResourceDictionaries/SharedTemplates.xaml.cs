0ed98e1c0a1cdfd2eeb9619ab57eaaee33cb216d
Daniel Paulino <dpaulino@users.noreply.github.com>
2022-11-29 22:41:33 -0800

    Moved flipview template to shared resource file

---
+++ src/AmbientSounds.Uwp/ResourceDictionaries/SharedTemplates.xaml.cs
@@ -0,0 +1,13 @@
+﻿// Required to support xbind
+// in resource dictionaries.
+// Ref: https://docs.microsoft.com/en-us/windows/uwp/data-binding/data-binding-in-depth#resource-dictionaries-with-xbind
+
+namespace AmbientSounds.ResourceDictionaries;
+
+public partial class SharedTemplates
+{
+    public SharedTemplates()
+    {
+        InitializeComponent();
+    }
+}
