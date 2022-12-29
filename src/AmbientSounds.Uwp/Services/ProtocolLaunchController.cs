f2b7b7e6953da8345e248b04eb807b1f2f716eff
Daniel Paulino <dpaulino@users.noreply.github.com>
2022-12-28 22:51:55 -0800

    Code cleanup

--- src/AmbientSounds.Uwp/Services/ProtocolLaunchController.cs
+++ src/AmbientSounds.Uwp/Services/ProtocolLaunchController.cs
@@ -4,44 +4,30 @@ using System;
 
 #nullable enable
 
-namespace AmbientSounds.Services
+namespace AmbientSounds.Services;
+
+public class ProtocolLaunchController
 {
-    public class ProtocolLaunchController
+    private readonly IMixMediaPlayerService _player;
+
+    private const string AutoPlayKey = "autoPlay";
+
+    public ProtocolLaunchController(IMixMediaPlayerService player)
     {
-        private readonly IMixMediaPlayerService _player;
-        private readonly INavigator _navigator;
+        Guard.IsNotNull(player);
 
-        private const string CompactKey = "compact";
-        private const string AutoPlayKey = "autoPlay";
+        _player = player;
+    }
 
-        public ProtocolLaunchController(
-            IMixMediaPlayerService player,
-            INavigator navigator)
+    public void ProcessLaunchProtocolArguments(string arguments)
+    {
+        var query = QueryString.Parse(arguments);
+        query.TryGetValue(AutoPlayKey, out var isAutoPlay);
+
+        if (!string.IsNullOrEmpty(isAutoPlay) && Convert.ToBoolean(isAutoPlay))
         {
-            Guard.IsNotNull(player, nameof(player));
-            Guard.IsNotNull(navigator, nameof(navigator));
-
-            _player = player;
-            _navigator = navigator;
-        }
-
-        public void ProcessLaunchProtocolArguments(string arguments)
-        {
-            var query = QueryString.Parse(arguments);
-            query.TryGetValue(CompactKey, out var isCompact);
-            query.TryGetValue(AutoPlayKey, out var isAutoPlay);
-
-            if (!string.IsNullOrEmpty(isCompact) && Convert.ToBoolean(isCompact))
-            {
-                // music player compact view not supported for now.
-                //_navigator.ToCompact();
-            }
-
-            if (!string.IsNullOrEmpty(isAutoPlay) && Convert.ToBoolean(isAutoPlay))
-            {
-                // Auto play music.
-                _player.Play();
-            }
+            // Auto play music.
+            _player.Play();
         }
     }
 }
