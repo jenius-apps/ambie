1dd73fc9b280491cc37c4d6dc8077f88952e4d96
Daniel Paulino <dpaulino@users.noreply.github.com>
2023-01-08 19:47:30 -0800

    Added telemetry for settings navigation

--- src/AmbientSounds.Uwp/Views/SettingsPage.xaml.cs
+++ src/AmbientSounds.Uwp/Views/SettingsPage.xaml.cs
@@ -1,30 +1,25 @@
-﻿using System;
+﻿using AmbientSounds.Constants;
+using AmbientSounds.Services;
+using Microsoft.Extensions.DependencyInjection;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+namespace AmbientSounds.Views;
 
-namespace AmbientSounds.Views
+public sealed partial class SettingsPage : Page
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class SettingsPage : Page
+    public SettingsPage()
     {
-        public SettingsPage()
+        this.InitializeComponent();
+    }
+
+    protected override void OnNavigatedTo(NavigationEventArgs e)
+    {
+        var telemetry = App.Services.GetRequiredService<ITelemetry>();
+        telemetry.TrackEvent(TelemetryConstants.PageNavTo, new Dictionary<string, string>
         {
-            this.InitializeComponent();
-        }
+            { "name", "settings" }
+        });
     }
 }
