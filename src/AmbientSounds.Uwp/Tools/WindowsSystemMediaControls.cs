d5cd5a4db4b75a0cb8e1245c9ec63730a9ab3ce4
Daniel Paulino <dpaulino@users.noreply.github.com>
2023-02-16 00:17:13 -0800

    Added comments

--- src/AmbientSounds.Uwp/Tools/WindowsSystemMediaControls.cs
+++ src/AmbientSounds.Uwp/Tools/WindowsSystemMediaControls.cs
@@ -9,6 +9,7 @@ public class WindowsSystemMediaControls : ISystemMediaControls
 {
     private readonly SystemMediaTransportControls _smtc;
 
+    /// <inheritdoc/>
     public event EventHandler<SystemMediaControlsButton>? ButtonPressed;
 
     public WindowsSystemMediaControls()
@@ -17,36 +18,42 @@ public class WindowsSystemMediaControls : ISystemMediaControls
         _smtc.ButtonPressed += SmtcButtonPressed;
     }
 
+    /// <inheritdoc/>
     public bool IsPlayEnabled
     {
         get => _smtc.IsPlayEnabled;
         set => _smtc.IsPlayEnabled = value;
     }
 
+    /// <inheritdoc/>
     public bool IsPauseEnabled
     {
         get => _smtc.IsPauseEnabled;
         set => _smtc.IsPauseEnabled = value;
     }
 
+    /// <inheritdoc/>
     public bool IsNextEnabled
     {
         get => _smtc.IsNextEnabled;
         set => _smtc.IsNextEnabled = value;
     }
 
+    /// <inheritdoc/>
     public bool IsPreviousEnabled
     {
         get => _smtc.IsPreviousEnabled;
         set => _smtc.IsPreviousEnabled = value;
     }
 
+    /// <inheritdoc/>
     public SystemMediaState PlaybackStatus
     {
         get => (SystemMediaState)(int)_smtc.PlaybackStatus;
         set => _smtc.PlaybackStatus = (MediaPlaybackStatus)(int)value;
     }
 
+    /// <inheritdoc/>
     public void UpdateDisplay(string title, string artist)
     {
         _smtc.DisplayUpdater.Type = MediaPlaybackType.Music;
--- src/AmbientSounds/Tools/IMediaPlayerFactory.cs
+++ src/AmbientSounds/Tools/IMediaPlayerFactory.cs
@@ -1,6 +1,17 @@
 ﻿namespace AmbientSounds.Tools;
 
+/// <summary>
+/// Factory interface that creates new media player objects.
+/// </summary>
 public interface IMediaPlayerFactory
 {
+    /// <summary>
+    /// Creates new player.
+    /// </summary>
+    /// <param name="disableDefaultSystemControls">
+    /// If true, this will ensure that 
+    /// the system controls wouldn't be automatically enabled.
+    /// </param>
+    /// <returns>Returns wrapper for media player.</returns>
     public IMediaPlayer CreatePlayer(bool disableDefaultSystemControls = false);
 }
