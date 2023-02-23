bb341da80aec2b95bae3932ab896cb9f359664dd
Daniel Paulino <dpaulino@users.noreply.github.com>
2023-02-22 20:30:40 -0800

    Fixed bug with focus session not updating UI

--- src/AmbientSounds/ViewModels/TimeBannerViewModel.cs
+++ src/AmbientSounds/ViewModels/TimeBannerViewModel.cs
@@ -3,6 +3,7 @@ using CommunityToolkit.Diagnostics;
 using AmbientSounds.Services;
 using JeniusApps.Common.Tools;
 using AmbientSounds.Extensions;
+using AmbientSounds.Tools;
 
 namespace AmbientSounds.ViewModels
 {
@@ -10,6 +11,7 @@ namespace AmbientSounds.ViewModels
     {
         private readonly IFocusService _focusService;
         private readonly ILocalizer _localizer;
+        private readonly IDispatcherQueue _dispatcherQueue;
 
         [ObservableProperty]
         private string _statusText = string.Empty;
@@ -19,20 +21,27 @@ namespace AmbientSounds.ViewModels
 
         public TimeBannerViewModel(
             IFocusService focusService,
-            ILocalizer localizer)
+            ILocalizer localizer,
+            IDispatcherQueue dispatcherQueue)
         {
-            Guard.IsNotNull(focusService, nameof(focusService));
-            Guard.IsNotNull(localizer, nameof(localizer));
+            Guard.IsNotNull(focusService);
+            Guard.IsNotNull(localizer);
+            Guard.IsNotNull(dispatcherQueue);
+
             _focusService = focusService;
             _localizer = localizer;
+            _dispatcherQueue = dispatcherQueue;
 
             _focusService.TimeUpdated += OnTimeUpdated;
         }
 
         private void OnTimeUpdated(object sender, FocusSession e)
         {
-            TimeText = e.Remaining.ToCountdownFormat();
-            StatusText = e.SessionType.ToDisplayString(_localizer);
+            _dispatcherQueue.TryEnqueue(() =>
+            {
+                TimeText = e.Remaining.ToCountdownFormat();
+                StatusText = e.SessionType.ToDisplayString(_localizer);
+            });
         }
     }
 }
