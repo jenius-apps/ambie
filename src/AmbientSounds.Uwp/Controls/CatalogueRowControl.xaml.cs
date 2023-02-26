a4b2caf67f028d28cbb2ea2c264c4e945cf88389
Daniel Paulino <dpaulino@users.noreply.github.com>
2023-02-25 20:25:03 -0800

    Adding programmatic scroll functionality

--- src/AmbientSounds.Uwp/Controls/CatalogueRowControl.xaml.cs
+++ src/AmbientSounds.Uwp/Controls/CatalogueRowControl.xaml.cs
@@ -3,6 +3,7 @@ using AmbientSounds.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
 #nullable enable
 
@@ -10,6 +11,9 @@ namespace AmbientSounds.Controls;
 
 public sealed partial class CatalogueRowControl : UserControl
 {
+    private const double ScrollOffset = 100d;
+    private ScrollViewer? _scrollViewer;
+
     public static readonly DependencyProperty RowDataProperty = DependencyProperty.Register(
         nameof(RowData),
         typeof(CatalogueRow),
@@ -38,5 +42,41 @@ public sealed partial class CatalogueRowControl : UserControl
         {
             await c.ViewModel.LoadAsync(row);
         }
+    }
+
+    private void OnRightClicked(object sender, RoutedEventArgs e)
+    {
+        _scrollViewer ??= FindVisualChild<ScrollViewer>(SoundListView);
+        _scrollViewer?.ChangeView(_scrollViewer.HorizontalOffset + ScrollOffset, null, null);
+    }
+
+    private void OnLeftClicked(object sender, RoutedEventArgs e)
+    {
+        _scrollViewer ??= FindVisualChild<ScrollViewer>(SoundListView);
+        _scrollViewer?.ChangeView(_scrollViewer.HorizontalOffset - ScrollOffset, null, null);
+    }
+
+    private static T? FindVisualChild<T>(DependencyObject? obj) where T : DependencyObject
+    {
+        for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+        {
+            DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+
+            if (child != null && child is T)
+            {
+                return (T)child;
+            }
+            else
+            {
+                T? childOfChild = FindVisualChild<T>(child);
+
+                if (childOfChild != null)
+                {
+                    return childOfChild;
+                }
+            }
+        }
+
+        return null;
     }
 }
