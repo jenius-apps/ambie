9b930ffbf01c7e166868a720c1dd317f905bf9db
Daniel Paulino <dpaulino@users.noreply.github.com>
2023-03-04 17:01:14 -0800

    Code cleanup

--- src/AmbientSounds/Repositories/IOnlineSoundRepository.cs
+++ src/AmbientSounds/Repositories/IOnlineSoundRepository.cs
@@ -2,33 +2,32 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace AmbientSounds.Repositories
+namespace AmbientSounds.Repositories;
+
+public interface IOnlineSoundRepository
 {
-    public interface IOnlineSoundRepository
-    {
-        /// <summary>
-        /// Retrieves the sound download link for
-        /// the specified sound data.
-        /// </summary>
-        /// <param name="s">The sound object whose download link will be fetched.</param>
-        /// <returns>URL to download the sound file.</returns>
-        Task<string> GetDownloadLinkAsync(Sound s);
+    /// <summary>
+    /// Retrieves the sound download link for
+    /// the specified sound data.
+    /// </summary>
+    /// <param name="s">The sound object whose download link will be fetched.</param>
+    /// <returns>URL to download the sound file.</returns>
+    Task<string> GetDownloadLinkAsync(Sound s);
 
-        /// <summary>
-        /// Retrieves list of sound data available online.
-        /// </summary>
-        /// <returns>A list of <see cref="Sound"/> instances.</returns>
-        Task<IReadOnlyList<Sound>> GetOnlineSoundsAsync();
+    /// <summary>
+    /// Retrieves list of sound data available online.
+    /// </summary>
+    /// <returns>A list of <see cref="Sound"/> instances.</returns>
+    Task<IReadOnlyList<Sound>> GetOnlineSoundsAsync();
 
-        /// <summary>
-        /// Retrieves list of sound data available online
-        /// based on given parameters.
-        /// </summary>
-        /// <param name="soundIds">The IDs of the sounds to fetch.</param>
-        /// <param name="iapId">The iap id filter to apply to the query.</param>
-        /// <returns>A list of <see cref="Sound"/> instances.</returns>
-        Task<IReadOnlyList<Sound>> GetOnlineSoundsAsync(
-            IReadOnlyList<string> soundIds,
-            string? iapId = null);
-    }
+    /// <summary>
+    /// Retrieves list of sound data available online
+    /// based on given parameters.
+    /// </summary>
+    /// <param name="soundIds">The IDs of the sounds to fetch.</param>
+    /// <param name="iapId">The iap id filter to apply to the query.</param>
+    /// <returns>A list of <see cref="Sound"/> instances.</returns>
+    Task<IReadOnlyList<Sound>> GetOnlineSoundsAsync(
+        IReadOnlyList<string> soundIds,
+        string? iapId = null);
 }
--- src/AmbientSounds/Services/ICatalogueService.cs
+++ src/AmbientSounds/Services/ICatalogueService.cs
@@ -2,20 +2,25 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace AmbientSounds.Services
-{
-    public interface ICatalogueService
-    {
-        /// <summary>
-        /// Retrieves download link for given sound.
-        /// </summary>
-        Task<string> GetDownloadLinkAsync(Sound s);
+namespace AmbientSounds.Services;
 
-        /// <summary>
-        /// Gets list of sounds that can be viewed
-        /// on the catalogue.
-        /// </summary>
-        Task<IReadOnlyList<Sound>> GetSoundsAsync();
-        Task<IReadOnlyList<Sound>> GetSoundsAsync(IReadOnlyList<string> soundIds);
-    }
+public interface ICatalogueService
+{
+    /// <summary>
+    /// Retrieves download link for given sound.
+    /// </summary>
+    Task<string> GetDownloadLinkAsync(Sound s);
+
+    /// <summary>
+    /// Gets list of sounds that can be viewed
+    /// on the catalogue.
+    /// </summary>
+    Task<IReadOnlyList<Sound>> GetSoundsAsync();
+
+    /// <summary>
+    /// Gets list of sounds based on given IDs.
+    /// </summary>
+    /// <param name="soundIds">List of sounds to fetch.</param>
+    /// <returns>List of sounds.</returns>
+    Task<IReadOnlyList<Sound>> GetSoundsAsync(IReadOnlyList<string> soundIds);
 }
