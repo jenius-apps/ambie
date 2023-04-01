bab1da3188754cb1a8e0b5938adbf0ac4fbb3ac0
Daniel Paulino <dpaulino@users.noreply.github.com>
2023-03-31 20:16:44 -0700

    Polished guide control

--- src/AmbientSounds.Uwp/Strings/en-US/Resources.generated.cs
+++ src/AmbientSounds.Uwp/Strings/en-US/Resources.generated.cs
@@ -1,5 +1,4 @@
 // File generated automatically by ReswPlus. https://github.com/DotNetPlus/ReswPlus
-// The NuGet package ReswPlusLib is necessary to support Pluralization.
 using System;
 using Windows.ApplicationModel.Resources;
 using Windows.UI.Xaml.Markup;
@@ -2329,6 +2328,19 @@ namespace AmbientSounds.Strings{
             }
         }
         #endregion
+
+        #region DescriptionText
+        /// <summary>
+        ///   Looks up a localized string similar to: Description
+        /// </summary>
+        public static string DescriptionText
+        {
+            get
+            {
+                return _resourceLoader.GetString("DescriptionText");
+            }
+        }
+        #endregion
     }
 
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("DotNetPlus.ReswPlus", "2.1.3")]
@@ -2518,6 +2530,7 @@ namespace AmbientSounds.Strings{
             InterruptionInsightsButtonText,
             SettingsOutputDevice,
             SettingsOutputDeviceDescription,
+            DescriptionText,
         }
 
         private static ResourceLoader _resourceLoader;
