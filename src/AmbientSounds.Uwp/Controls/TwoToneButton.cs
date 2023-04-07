0715179b0d37a6bfc05c5a4d4bfea2ce9fc19973
Daniel Paulino <dpaulino@users.noreply.github.com>
2023-04-06 20:38:03 -0700

    Added two tone button

---
+++ src/AmbientSounds.Uwp/Controls/TwoToneButton.cs
@@ -0,0 +1,40 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+#nullable enable
+
+namespace AmbientSounds.Controls;
+
+public sealed class TwoToneButton : Button
+{
+    public static readonly DependencyProperty PrimaryTextProperty =
+        DependencyProperty.Register(
+            "PrimaryText",
+            typeof(string),
+            typeof(TwoToneButton),
+            new PropertyMetadata(string.Empty));
+
+    public static readonly DependencyProperty SecondaryTextProperty =
+        DependencyProperty.Register(
+            "SecondaryText",
+            typeof(string),
+            typeof(TwoToneButton),
+            new PropertyMetadata(string.Empty));
+
+    public TwoToneButton()
+    {
+        this.DefaultStyleKey = typeof(TwoToneButton);
+    }
+
+    public string PrimaryText
+    {
+        get => (string)GetValue(PrimaryTextProperty);
+        set => SetValue(PrimaryTextProperty, value);
+    }
+
+    public string SecondaryText
+    {
+        get => (string)GetValue(SecondaryTextProperty);
+        set => SetValue(SecondaryTextProperty, value);
+    }
+}
