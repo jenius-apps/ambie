056992803ac4ee0570c14683fbacf0650ca09a64
Daniel <dpaulino@users.noreply.github.com>
2023-04-06 22:24:40 -0700

    Adding two tone button (#353)
    * Added two tone button
    
    * Converted two tone button to use content presenter
    
    * Using two tone button
    
    * Temporarily disable tab stop

---
+++ src/AmbientSounds.Uwp/Controls/TwoToneButton.cs
@@ -0,0 +1,27 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+#nullable enable
+
+namespace AmbientSounds.Controls;
+
+public sealed class TwoToneButton : Button
+{
+    public static readonly DependencyProperty SecondaryContentProperty =
+        DependencyProperty.Register(
+            nameof(SecondaryContent),
+            typeof(object),
+            typeof(TwoToneButton),
+            new PropertyMetadata(null));
+
+    public TwoToneButton()
+    {
+        this.DefaultStyleKey = typeof(TwoToneButton);
+    }
+
+    public object? SecondaryContent
+    {
+        get => GetValue(SecondaryContentProperty);
+        set => SetValue(SecondaryContentProperty, value);
+    }
+}
