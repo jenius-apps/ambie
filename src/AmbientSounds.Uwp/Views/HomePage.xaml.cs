351963709b873c2e87c1adea5b27faed2ee0dfc2
Daniel Paulino <dpaulino@users.noreply.github.com>
2023-05-04 22:31:20 -0700

    Adding home page telemetry

--- src/AmbientSounds.Uwp/Views/HomePage.xaml.cs
+++ src/AmbientSounds.Uwp/Views/HomePage.xaml.cs
@@ -1,30 +1,26 @@
-﻿using System;
+﻿using AmbientSounds.Constants;
+using JeniusApps.Common.Telemetry;
+using Microsoft.Extensions.DependencyInjection;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+namespace AmbientSounds.Views;
 
-namespace AmbientSounds.Views
+public sealed partial class HomePage : Page
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class HomePage : Page
+    public HomePage()
     {
-        public HomePage()
-        {
-            this.InitializeComponent();
-        }
+        this.InitializeComponent();
+    }
+
+    protected override void OnNavigatedTo(NavigationEventArgs e)
+    {
+        App.Services.GetRequiredService<ITelemetry>().TrackEvent(
+            TelemetryConstants.PageNavTo,
+            new Dictionary<string, string>
+            {
+                { "name", "home" }
+            });
     }
 }
