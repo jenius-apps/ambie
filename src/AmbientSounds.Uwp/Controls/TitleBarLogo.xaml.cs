9239670660f40d5052e3dbec9f4a3b950971b101
Jaiganésh Kumaran <jaiganesh.kumaran@outlook.com>
2023-06-04 11:31:31 +0530

    Dim title bar when unfocused (#360)
    * Dim title bar when unfocused
    
    * Ensure setting title text foreground always on startup
    
    * Revert solution file changes
    
    * Delete AmbientSounds.sln
    
    * Revert deletion of solution file
    
    * Remove extra line in AmbientSounds.sln
    
    * Use state trigger to change title bar text foreground
    
    * Revert changes to solution and project file
    
    * Update src/AmbientSounds.Uwp/Controls/TitleBarLogo.xaml.cs
    
    Co-authored-by: Daniel <dpaulino@users.noreply.github.com>
    
    * Update src/AmbientSounds.Uwp/Controls/TitleBarLogo.xaml
    
    Co-authored-by: Daniel <dpaulino@users.noreply.github.com>
    
    * Update src/AmbientSounds.Uwp/Controls/TitleBarLogo.xaml
    
    Co-authored-by: Daniel <dpaulino@users.noreply.github.com>
    
    * Fix memory leak by unregistering Window.Current.Activated
    
    * Revert changes to the solution file
    
    ---------
    
    Co-authored-by: Daniel <dpaulino@users.noreply.github.com>

--- src/AmbientSounds.Uwp/Controls/TitleBarLogo.xaml.cs
+++ src/AmbientSounds.Uwp/Controls/TitleBarLogo.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Windows.UI.Xaml;
+﻿using Windows.UI.Core;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace AmbientSounds.Controls;
@@ -12,14 +13,42 @@ public sealed partial class TitleBarLogo : UserControl
             typeof(TitleBarLogo),
             new PropertyMetadata(Strings.Resources.AppDisplayName));
 
+    public static readonly DependencyProperty IsWindowFocusedProperty =
+        DependencyProperty.Register(
+            nameof(IsWindowFocused),
+            typeof(bool), 
+            typeof(TitleBarLogo),
+            new PropertyMetadata(false));
+
     public TitleBarLogo()
     {
         this.InitializeComponent();
+    }
+
+    public bool IsWindowFocused
+    {
+        get => (bool)GetValue(IsWindowFocusedProperty);
+        set => SetValue(IsWindowFocusedProperty, value);
     }
 
     public string DisplayText
     {
         get => (string)GetValue(DisplayTextProperty);
         set => SetValue(DisplayTextProperty, value);
+    }
+
+    private void Window_Activated(object sender, WindowActivatedEventArgs e)
+    {
+        IsWindowFocused = e.WindowActivationState != CoreWindowActivationState.Deactivated;
+    }
+
+    private void UserControl_Loaded(object sender, RoutedEventArgs e)
+    {
+        Window.Current.Activated += Window_Activated;
+    }
+
+    private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+    {
+        Window.Current.Activated -= Window_Activated;
     }
 }
