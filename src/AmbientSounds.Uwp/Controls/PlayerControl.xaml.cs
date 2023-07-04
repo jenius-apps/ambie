6018d9c9c3413a613a991b8b9f3f9fc73c94ccea
Daniel Paulino <dpaulino@users.noreply.github.com>
2023-07-03 22:26:56 -0700

    Updated icons

--- src/AmbientSounds.Uwp/Controls/PlayerControl.xaml.cs
+++ src/AmbientSounds.Uwp/Controls/PlayerControl.xaml.cs
@@ -31,23 +31,5 @@ namespace AmbientSounds.Controls
             get => (bool)GetValue(IsCompactProperty);
             set => SetValue(IsCompactProperty, value);
         }
-
-        private string GetDynamicIcon(double volume)
-        {
-            if (volume > 70)
-            {
-                return "\uEB7D";
-            }
-            else if (volume > 30)
-            {
-                return "\uEB7C";
-            }
-            else if (volume >= 1)
-            {
-                return "\uEB7B";
-            }
-
-            return "\uEB80";
-        }
     }
 }
