6e6afd0ff66de083ae7383b36f7d95f70a768ef9
Daniel Paulino <dpaulino@users.noreply.github.com>
2023-10-26 20:32:43 -0700

    Added task project

---
+++ src/AmbientSounds.Tasks/StartupTask.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Windows.ApplicationModel.Background;
+
+namespace AmbientSounds.Tasks;
+
+public sealed class StartupTask : IBackgroundTask
+{
+    public void Run(IBackgroundTaskInstance taskInstance)
+    {
+        throw new NotImplementedException();
+    }
+}
