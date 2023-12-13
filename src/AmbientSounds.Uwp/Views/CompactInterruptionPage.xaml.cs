63a5c9ba09bb83efd51e03a8c5a421297b851cae
Daniel Paulino <dpaulino@users.noreply.github.com>
2023-12-12 21:00:47 -0800

    Updating interruption page

--- src/AmbientSounds.Uwp/Views/CompactInterruptionPage.xaml.cs
+++ src/AmbientSounds.Uwp/Views/CompactInterruptionPage.xaml.cs
@@ -6,9 +6,6 @@ using Windows.UI.Xaml.Controls;
 
 namespace AmbientSounds.Views;
 
-/// <summary>
-/// An empty page that can be used on its own or navigated to within a Frame.
-/// </summary>
 public sealed partial class CompactInterruptionPage : Page
 {
     public CompactInterruptionPage()
