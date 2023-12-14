efb7f4838e301bbaca53c41b5d46ba404d935ebd
Daniel Paulino <dpaulino@users.noreply.github.com>
2023-12-13 21:47:18 -0800

    Added visibility logic

--- src/AmbientSounds.Uwp/Controls/TaskTicker.xaml.cs
+++ src/AmbientSounds.Uwp/Controls/TaskTicker.xaml.cs
@@ -1,19 +1,6 @@
 ﻿using AmbientSounds.ViewModels;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 #nullable enable
 
