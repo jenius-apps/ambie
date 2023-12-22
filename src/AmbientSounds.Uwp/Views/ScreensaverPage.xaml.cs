1712fccf6ace9488cdaa8ed4af8b8b580403b686
Daniel Paulino <dpaulino@users.noreply.github.com>
2023-12-22 08:49:23 -0800

    Removing animated screensavers

--- src/AmbientSounds.Uwp/Views/ScreensaverPage.xaml.cs
+++ src/AmbientSounds.Uwp/Views/ScreensaverPage.xaml.cs
@@ -1,16 +1,10 @@
 ﻿using AmbientSounds.Constants;
-using AmbientSounds.Effects;
 using AmbientSounds.Services;
-using AmbientSounds.Shaders;
 using AmbientSounds.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Graphics.Canvas;
-using Microsoft.Graphics.Canvas.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using Windows.Foundation;
 using Windows.Media.Core;
 using Windows.System;
 using Windows.System.Display;
@@ -30,8 +24,6 @@ public sealed partial class ScreensaverPage : Page
 {
     private readonly DisplayRequest _displayRequest;
     private readonly DispatcherQueue _dispatcherQueue;
-    //private AnimatedWallpaperEffect? _animatedWallpaperEffect;
-    //private double _resolutionScale;
 
     public ScreensaverPage()
     {
@@ -44,11 +36,6 @@ public sealed partial class ScreensaverPage : Page
         ViewModel.PropertyChanged += OnViewModelPropertyChanged;
         _displayRequest = new DisplayRequest();
         _dispatcherQueue = DispatcherQueue.GetForCurrentThread();
-
-        // Set the wallpapers to run at 24fps to save resources (the animations are very slow, so not noticeable)
-        //WallpaperCanvasControl.TargetElapsedTime = TimeSpan.FromSeconds(1 / 24.0f);
-
-        Unloaded += ScreensaverPage_Unloaded;
     }
 
     public ScreensaverPageViewModel ViewModel => (ScreensaverPageViewModel)this.DataContext;
@@ -109,18 +96,6 @@ public sealed partial class ScreensaverPage : Page
         _displayRequest.RequestRelease();
     }
 
-    private void ScreensaverPage_Unloaded(object sender, RoutedEventArgs e)
-    {
-        // Remove the canvas from the visual tree manually to avoid memory leaks.
-        // See: https://microsoft.github.io/Win2D/WinUI2/html/RefCycles.htm.
-        //WallpaperCanvasControl.Draw -= CanvasAnimatedControl_Draw;
-        //WallpaperCanvasControl.RemoveFromVisualTree();
-        //WallpaperCanvasControl = null;
-
-        //// Also dispose the effect to remove pressure from the GC
-        //_animatedWallpaperEffect?.Dispose();
-    }
-
     private void OnViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
     {
         if (e.PropertyName == nameof(ViewModel.VideoSource))
@@ -128,10 +103,6 @@ public sealed partial class ScreensaverPage : Page
             VideoPlayer.MediaPlayer.IsLoopingEnabled = true;
             VideoPlayer.MediaPlayer.Source = MediaSource.CreateFromUri(ViewModel.VideoSource);
         }
-        //else if (e.PropertyName == nameof(ViewModel.AnimatedBackgroundName))
-        //{
-        //    SetupAnimatedShaderProperties();
-        //}
     }
 
     private void OnViewModelLoaded(object sender, EventArgs e)
@@ -165,8 +136,6 @@ public sealed partial class ScreensaverPage : Page
 
             SettingsFlyout.Items.Add(menuItem);
         }
-
-        //SetupAnimatedShaderProperties();
     }
 
     private void OnMenuItemClicked(object sender, RoutedEventArgs e)
@@ -291,79 +260,4 @@ public sealed partial class ScreensaverPage : Page
 
         InactiveTimer?.Start();
     }
-
-    /// <summary>
-    /// Configures the shader runner and resolution scale when the control is loaded or the selected shader changes.
-    /// </summary>
-    //private void SetupAnimatedShaderProperties()
-    //{
-    //    string? animatedBackgroundName = ViewModel.AnimatedBackgroundName;
-
-    //    // Dispose the existing effect, if there is one
-    //    _animatedWallpaperEffect?.Dispose();
-
-    //    // We need explicit references to all type to help the .NET Native linker resolve all type dependencies
-    //    _animatedWallpaperEffect = animatedBackgroundName switch
-    //    {
-    //        nameof(ColorfulInfinity) => new AnimatedWallpaperEffect.For<ColorfulInfinity>((width, height, time) => new ColorfulInfinity((float)time.TotalSeconds / 16f, new int2(width, height))),
-    //        nameof(ProteanClouds) => new AnimatedWallpaperEffect.For<ProteanClouds>((width, height, time) => new ProteanClouds((float)time.TotalSeconds / 16f, new int2(width, height))),
-    //        _ => null
-    //    };
-
-    //    // Configure the resolution scale, to save GPU computation. The scale is picked to
-    //    // maintain enough visual quality, so it depends on the visuals of each shader.
-    //    // In general, shaders with more fine grained details need a higher resolution.
-    //    _resolutionScale = animatedBackgroundName switch
-    //    {
-    //        nameof(ColorfulInfinity) => 0.5,
-    //        nameof(ProteanClouds) => 0.4,
-    //        _ => 1.0
-    //    };
-    //}
-
-    //private void CanvasAnimatedControl_Draw(ICanvasAnimatedControl sender, CanvasAnimatedDrawEventArgs args)
-    //{
-    //    // We do need an effect to draw (which should be available)
-    //    if (_animatedWallpaperEffect is not { } animatedWallpaperEffect)
-    //    {
-    //        return;
-    //    }
-
-    //    try
-    //    {
-    //        double resolutionScale = _resolutionScale;
-    //        Size canvasSize = sender.Size;
-    //        Size renderSize = new(canvasSize.Width * resolutionScale, canvasSize.Height * resolutionScale);
-
-    //        // Set the constant buffer
-    //        animatedWallpaperEffect.ElapsedTime = args.Timing.TotalTime;
-    //        animatedWallpaperEffect.ScreenWidth = sender.ConvertDipsToPixels((float)renderSize.Width, CanvasDpiRounding.Round);
-    //        animatedWallpaperEffect.ScreenHeight = sender.ConvertDipsToPixels((float)renderSize.Height, CanvasDpiRounding.Round);
-
-    //        // Draw the shader with the requested resolution scale
-    //        args.DrawingSession.DrawImage(
-    //            image: animatedWallpaperEffect,
-    //            destinationRectangle: new Rect(0, 0, canvasSize.Width, canvasSize.Height),
-    //            sourceRectangle: new Rect(0, 0, renderSize.Width, renderSize.Height));
-    //    }
-    //    catch (Exception e)
-    //    {
-    //        // Pause rendering
-    //        sender.Paused = true;
-
-    //        // Log the error to telemetry
-    //        App.Services.GetRequiredService<ITelemetry>().TrackError(e, new Dictionary<string, string>()
-    //        {
-    //            { "name", animatedWallpaperEffect.EffectName },
-    //            { "deviceLostReason", $"0x{args.DrawingSession.Device.GetDeviceLostReason():X8}" }
-    //        });
-
-    //        // Show the error banner. For this we need to first move back to the UI thread,
-    //        // as the drawing handlers are invoked by a render thread spun up by Win2D.
-    //        _ = _dispatcherQueue.TryEnqueue(() =>
-    //        {
-    //            ((InfoBar)FindName(nameof(RenderingErrorInfoBar))).IsOpen = true;
-    //        });
-    //    }
-    //}
 }
