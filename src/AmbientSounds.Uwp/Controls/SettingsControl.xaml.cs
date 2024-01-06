5df60dd611ba7394d746b5df78645f673a8c0f3d
Daniel Paulino <dpaulino@users.noreply.github.com>
2024-01-05 21:54:34 -0800

    Making experimental features hidden

--- src/AmbientSounds.Uwp/Controls/SettingsControl.xaml.cs
+++ src/AmbientSounds.Uwp/Controls/SettingsControl.xaml.cs
@@ -1,42 +1,48 @@
 ﻿using AmbientSounds.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Toolkit.Uwp.Helpers;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 
 #nullable enable
 
-namespace AmbientSounds.Controls
+namespace AmbientSounds.Controls;
+
+public sealed partial class SettingsControl : UserControl
 {
-    public sealed partial class SettingsControl : UserControl
+    public SettingsControl()
     {
-        public SettingsControl()
+        this.InitializeComponent();
+        this.DataContext = App.Services.GetRequiredService<SettingsViewModel>();
+    }
+
+    public SettingsViewModel ViewModel => (SettingsViewModel)this.DataContext;
+
+    private string Version => SystemInformation.Instance.ApplicationVersion.ToFormattedString();
+
+    public void Initialize() => ViewModel.Initialize();
+
+    public void Uninitialize() => ViewModel.Uninitialize();
+
+    private void OnImageClicked(object sender, ItemClickEventArgs e)
+    {
+        if (e.ClickedItem is string imagePath)
         {
-            this.InitializeComponent();
-            this.DataContext = App.Services.GetRequiredService<SettingsViewModel>();
+            ViewModel.SelectImageCommand.Execute(imagePath);
         }
+    }
 
-        public SettingsViewModel ViewModel => (SettingsViewModel)this.DataContext;
-
-        private string Version => SystemInformation.Instance.ApplicationVersion.ToFormattedString();
-
-        public void Initialize() => ViewModel.Initialize();
-
-        public void Uninitialize() => ViewModel.Uninitialize();
-
-        private void OnImageClicked(object sender, ItemClickEventArgs e)
+    private void OnThemeSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (e.AddedItems is [ComboBoxItem c, ..] && c.Tag is string s)
         {
-            if (e.ClickedItem is string imagePath)
-            {
-                ViewModel.SelectImageCommand.Execute(imagePath);
-            }
+            ViewModel.UpdateTheme(s);
         }
+    }
 
-        private void OnThemeSelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (e.AddedItems is [ComboBoxItem c, ..] && c.Tag is string s)
-            {
-                ViewModel.UpdateTheme(s);
-            }
-        }
+    private void OnDoubleTapped(object sender, DoubleTappedRoutedEventArgs e)
+    {
+        ExperimentalPanel.Visibility = Visibility.Visible;
     }
 }
