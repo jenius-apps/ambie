6873f6126f1a9b5fcab5ca0d6816df714d112070
Daniel Paulino <dpaulino@users.noreply.github.com>
2024-04-06 18:51:26 -0700

    Added page navigation and animation to catalogue page

--- src/AmbientSounds.Uwp/Views/XboxShellPage.xaml.cs
+++ src/AmbientSounds.Uwp/Views/XboxShellPage.xaml.cs
@@ -1,6 +1,8 @@
-﻿using AmbientSounds.ViewModels;
+﻿using AmbientSounds.Services;
+using AmbientSounds.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
 
 namespace AmbientSounds.Views;
 
@@ -13,4 +15,15 @@ public sealed partial class XboxShellPage : Page
     }
 
     public XboxShellPageViewModel ViewModel => (XboxShellPageViewModel)this.DataContext;
+
+    private async void OnMoreSoundsClicked(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+    {
+        if (App.Services.GetRequiredService<INavigator>().RootFrame is Frame root)
+        {
+            _ = FadeOutAnimation.StartAsync();
+            _ = ActionBarExitAnimation.StartAsync();
+            await SoundGridExitAnimation.StartAsync();
+            root.Navigate(typeof(CataloguePage), null, new SuppressNavigationTransitionInfo());
+        }
+    }
 }
