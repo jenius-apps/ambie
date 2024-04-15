f56df9e9fe4e4ba37f4d86f89ef278bb94e7ad65
Daniel Paulino <dpaulino@users.noreply.github.com>
2024-04-14 22:38:41 -0700

    Added slideshowmode

---
+++ src/AmbientSounds/Models/SlideshowMode.cs
@@ -0,0 +1,23 @@
+﻿namespace AmbientSounds.Models;
+
+/// <summary>
+/// Display modes used for the slideshow experience on Xbox.
+/// </summary>
+public enum SlideshowMode
+{
+    /// <summary>
+    /// Represents displaying images in a slideshow.
+    /// </summary>
+    Images,
+
+    /// <summary>
+    /// Represents the user's desire to display nothing on screen,
+    /// preferring a dark screen instead.
+    /// </summary>
+    DarkScreen,
+
+    /// <summary>
+    /// Represents the user's desire to display videos, if available.
+    /// </summary>
+    Video
+}
