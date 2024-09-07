c7d99b18b538668e707cc2ccdd2851a8685f0ad9
Daniel Paulino <dpaulino@users.noreply.github.com>
2024-09-06 20:32:58 -0700

    WIP

---
+++ src/AmbientSounds.Uwp/Tools/CacheableHttpClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+using Windows.Web.Http;
+
+namespace AmbientSounds.Tools.Uwp;
+
+internal class CacheableHttpClient : ICacheableHttpClient
+{
+    private readonly HttpClient _client;
+
+    public CacheableHttpClient()
+    {
+        _client = new HttpClient();
+    }
+
+    /// <inheritdoc/>
+    public async Task<Stream> GetStreamAsync(string url)
+    {
+        IInputStream inputStream = await _client.GetInputStreamAsync(new Uri(url)).AsTask();
+        return inputStream.AsStreamForRead();
+    }
+
+    /// <inheritdoc/>
+    public async Task<string> GetStringAsync(string url)
+    {
+        return await _client.GetStringAsync(new Uri(url)).AsTask();
+    }
+}
---
+++ src/AmbientSounds/Tools/ICacheableHttpClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmbientSounds.Tools;
+
+public interface ICacheableHttpClient
+{
+    Task<string> GetStringAsync(string? url);
+
+    Task<Stream> GetStreamAsync(string? url);
+}
