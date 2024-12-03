29b8e40be445b1b6797d9b21f7034009f6037889
Daniel Paulino <dpaulino@users.noreply.github.com>
2024-12-03 09:28:00 -0800

    initial commit

---
+++ src/AmbientSounds.Uwp/Tools/WindowsPushNotificationRegistrar.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AmbientSounds.Constants;
+using AmbientSounds.Services;
+using JeniusApps.Common.Settings;
+using JeniusApps.Common.Telemetry;
+using Windows.Networking.PushNotifications;
+
+#nullable enable
+
+namespace AmbientSounds.Tools.Uwp;
+
+public sealed class WindowsPushNotificationRegistrar : IPushNotificationRegistrar
+{
+    private readonly IUserSettings _userSettings;
+    private readonly ITelemetry _telemetry;
+
+    public WindowsPushNotificationRegistrar(
+     IUserSettings userSettings,
+     ITelemetry telemetry)
+    {
+        _userSettings = userSettings;
+        _telemetry = telemetry;
+    }
+
+    public async Task<bool> RegisterAsync()
+    {
+        PushNotificationChannel? channel = await PushNotificationChannelManager.CreatePushNotificationChannelForApplicationAsync();
+        if (channel is not { Uri.Length: > 0 })
+        {
+            return false;
+        }
+        
+        // upload channel URI to service.
+    }
+
+    public async Task<bool> TryRegisterBasedOnUserSettingsAsync()
+    {
+        if (_userSettings.Get<bool>(UserSettingsConstants.Notifications))
+        {
+            return await RegisterAsync();
+        }
+
+        return false;
+    }
+
+    public Task UnregiserAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
