633115278e457533111a5830edcebbb79ef4bdc2
Daniel <dpaulino@users.noreply.github.com>
2024-12-16 22:37:54 -0800

    Wns (#455)
    * Reworking how notifications work
    
    * Added service bus class
    
    * Added serializer

---
+++ src/AmbientSounds/Tools/IPushNotificationService.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace AmbientSounds.Tools;
+
+/// <summary>
+/// Main interface for orchestrating push notification registration.
+/// </summary>
+public interface IPushNotificationService
+{
+    /// <summary>
+    /// Registers the given device for push notifications.
+    /// </summary>
+    /// <param name="deviceId">A unique ID representing this device. Preferably a GUID.</param>
+    /// <param name="primaryLanguageCode">A two-letter ISO language code representing the user's primary language.</param>
+    /// <param name="ct">A cancellation token.</param>
+    /// <returns>True if successful, false otherwise.</returns>
+    Task<bool> RegisterAsync(string deviceId, string primaryLanguageCode, CancellationToken ct);
+
+    /// <summary>
+    /// Unregisters the given device from notifications.
+    /// </summary>
+    /// <param name="deviceId">A unique ID representing this device. Preferably a GUID.</param>
+    /// <param name="ct">A cancellation token.</param>
+    Task UnregisterAsync(string deviceId, CancellationToken ct);
+}
