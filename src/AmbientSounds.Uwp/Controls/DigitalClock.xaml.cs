513b0a876ba753d6beb652db564687a946ca262d
Daniel Paulino <dpaulino@users.noreply.github.com>
2024-12-23 16:10:24 -0800

    Added clock control

---
+++ src/AmbientSounds.Uwp/Controls/DigitalClock.xaml.cs
@@ -0,0 +1,51 @@
+﻿using JeniusApps.Common.Tools;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Timers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+#nullable enable
+
+namespace AmbientSounds.Controls;
+
+public sealed partial class DigitalClock : UserControl
+{
+    private readonly IDispatcherQueue _dispatcher;
+
+    private readonly Timer _timer = new()
+    {
+        Interval = 1000 // milliseconds
+    };
+
+    private static readonly DependencyProperty TimeTextProperty = DependencyProperty.Register(
+        nameof(TimeText),
+        typeof(string),
+        typeof(DigitalClock),
+        new PropertyMetadata(string.Empty));
+
+    public DigitalClock()
+    {
+        this.InitializeComponent();
+        _dispatcher = App.Services.GetRequiredService<IDispatcherQueue>();
+        UpdateTimeText();
+        _timer.Elapsed += OnTimerElapsed;
+        _timer.Start();
+    }
+
+    private string TimeText
+    {
+        get => (string)GetValue(TimeTextProperty);
+        set => SetValue(TimeTextProperty, value);
+    }
+
+    private void OnTimerElapsed(object sender, ElapsedEventArgs e) => UpdateTimeText();
+
+    private void UpdateTimeText()
+    {
+        _dispatcher.TryEnqueue(() =>
+        {
+            TimeText = DateTime.Now.ToLongTimeString();
+        });
+    }
+}
