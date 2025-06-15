409ab58d142977489f76a066d9ab4d1e6a17da6c
Daniel Paulino <dpaulino@users.noreply.github.com>
2025-06-14 18:00:31 -0700

    Added stub renewal task

---
+++ src/AmbientSounds.Tasks/PushNotificationRenewalTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+namespace AmbientSounds.Tasks;
+
+public sealed class PushNotificationRenewalTask : IBackgroundTask
+{
+    public void Run(IBackgroundTaskInstance taskInstance)
+    {
+        throw new NotImplementedException();
+    }
+}
