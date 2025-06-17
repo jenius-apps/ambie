5ea3abb7a2af54994207add89bb11178df2b44b8
Daniel Paulino <dpaulino@users.noreply.github.com>
2025-06-16 20:15:55 -0700

    Added logic, but not working yet

--- src/AmbientSounds.Tasks/PushNotificationRenewalTask.cs
+++ src/AmbientSounds.Tasks/PushNotificationRenewalTask.cs
@@ -1,16 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using AmbientSounds.Constants;
+using AmbientSounds.Services;
+using AmbientSounds.Tools;
+using JeniusApps.Common.Settings;
+using JeniusApps.Common.Settings.Uwp;
+using Microsoft.Extensions.DependencyInjection;
 using Windows.ApplicationModel.Background;
+
+#nullable enable
 
 namespace AmbientSounds.Tasks;
 
 public sealed class PushNotificationRenewalTask : IBackgroundTask
 {
-    public void Run(IBackgroundTaskInstance taskInstance)
+    private IServiceProvider? _serviceProvider;
+
+    public async void Run(IBackgroundTaskInstance taskInstance)
     {
-        throw new NotImplementedException();
+        var d = taskInstance.GetDeferral();
+        _serviceProvider = ConfigureServices();
+
+        IUserSettings userSettings = _serviceProvider.GetRequiredService<IUserSettings>();
+
+        if (userSettings.Get<bool>(UserSettingsConstants.Notifications) is false ||
+            userSettings.Get<string>(UserSettingsConstants.LocalUserIdKey) is not { Length: > 0 } id)
+        {
+            return;
+        }
+
+        try
+        {
+#if DEBUG
+            // Don't want to needlessly send messages to the notification service
+            // when in debug mode.
+            await Task.Delay(1);
+#else
+            await Services.GetRequiredService<Tools.IPushNotificationService>().RegisterAsync(
+                id,
+                Services.GetRequiredService<JeniusApps.Common.Tools.ISystemInfoProvider>().GetCulture(),
+                default);
+#endif
+        }
+        catch { }
+
+        d.Complete();
     }
+
+    private static IServiceProvider ConfigureServices()
+    {
+        ServiceCollection collection = new();
+        ConfigureServices(collection);
+
+        collection.AddSingleton<IUserSettings, LocalSettings>(s => new LocalSettings(UserSettingsConstants.Defaults));
+        collection.AddSingleton<IPushNotificationStorage, AzureServiceBusPushNotificationStorage>(s =>
+        {
+            var connectionString = s.GetRequiredService<IAppSettings>().NotificationHubConnectionString;
+            var queueName = s.GetRequiredService<IAppSettings>().NotificationHubName;
+            return new AzureServiceBusPushNotificationStorage(connectionString, queueName);
+        });
+
+        IServiceProvider provider = collection.BuildServiceProvider();
+        return provider;
+
+    }
+
+    [Singleton(typeof(PushNotificationService), typeof(IPushNotificationService))]
+    [Singleton(typeof(WindowsPushNotificationSource), typeof(IPushNotificationSource))]
+    private static partial void ConfigureServices(IServiceCollection services);
 }
