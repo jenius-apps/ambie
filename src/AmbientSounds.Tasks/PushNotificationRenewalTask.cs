014b6daba584ef9f2d74a6ed1eaf771510c542e6
Daniel <dpaulino@users.noreply.github.com>
2025-06-17 18:42:35 -0700

    Adding push renewal background task (#472)
    * Added stub renewal task
    
    * Added logic to toggle renewal bg task
    
    * Added logic, but not working yet
    
    * Moved push notification logic to common repo
    
    * Added logic for push renewal task
    
    * Adding registration logic
    
    * Deduplicating logic
    
    * Fixing crash with background task

---
+++ src/AmbientSounds.Tasks/PushNotificationRenewalTask.cs
@@ -0,0 +1,57 @@
+﻿using AmbientSounds.Constants;
+using AmbientSounds.Services;
+using CommunityToolkit.Extensions.DependencyInjection;
+using JeniusApps.Common.PushNotifications;
+using JeniusApps.Common.PushNotifications.Uwp;
+using JeniusApps.Common.Settings;
+using JeniusApps.Common.Settings.Uwp;
+using JeniusApps.Common.Tools;
+using JeniusApps.Common.Tools.Uwp;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Windows.ApplicationModel.Background;
+using Windows.ApplicationModel.Resources;
+
+#nullable enable
+
+namespace AmbientSounds.Tasks;
+
+public sealed partial class PushNotificationRenewalTask : IBackgroundTask
+{
+    private IServiceProvider? _serviceProvider;
+
+    public async void Run(IBackgroundTaskInstance taskInstance)
+    {
+        var d = taskInstance.GetDeferral();
+        _serviceProvider = ConfigureServices();
+
+        _ = await _serviceProvider.GetRequiredService<IPushNotificationRegistrationService>().TryRegisterPushNotificationsAsync();
+
+        d.Complete();
+    }
+
+    private static IServiceProvider ConfigureServices()
+    {
+        var resourceLoader = ResourceLoader.GetForViewIndependentUse("appsettings");
+        ServiceCollection collection = new();
+        ConfigureServices(collection);
+
+        collection.AddSingleton<IUserSettings, LocalSettings>(s => new LocalSettings(UserSettingsConstants.Defaults));
+        collection.AddSingleton<IPushNotificationStorage, AzureServiceBusPushNotificationStorage>(s =>
+        {
+            var connectionString = resourceLoader.GetString("NotificationHubConnectionString");
+            var queueName = resourceLoader.GetString("NotificationHubName");
+            return new AzureServiceBusPushNotificationStorage(connectionString, queueName);
+        });
+
+        IServiceProvider provider = collection.BuildServiceProvider();
+        return provider;
+
+    }
+
+    [Singleton(typeof(PushNotificationRegistrationService), typeof(IPushNotificationRegistrationService))]
+    [Singleton(typeof(SystemInfoProvider), typeof(ISystemInfoProvider))]
+    [Singleton(typeof(PushNotificationService), typeof(IPushNotificationService))]
+    [Singleton(typeof(WindowsPushNotificationSource), typeof(IPushNotificationSource))]
+    private static partial void ConfigureServices(IServiceCollection services);
+}
--- src/AmbientSounds.Uwp/Services/BackgroundTaskService.cs
+++ src/AmbientSounds.Uwp/Services/BackgroundTaskService.cs
@@ -17,6 +17,7 @@ public class BackgroundTaskService : IBackgroundTaskService
         }
     }
 
+    /// <inheritdoc/>
     public async Task<bool> RequestPermissionAsync()
     {
         var result = await BackgroundExecutionManager.RequestAccessAsync();
@@ -24,6 +25,28 @@ public class BackgroundTaskService : IBackgroundTaskService
             or BackgroundAccessStatus.AllowedSubjectToSystemPolicy;
     }
 
+    /// <inheritdoc/>
+    public void TogglePushNotificationRenewalTask(bool enable)
+    {
+        var taskType = typeof(PushNotificationRenewalTask);
+
+        // To avoid duplicate registrations, always unregister no matter the "enable" value.
+        UnregisterTask(taskType.Name);
+
+        if (enable)
+        {
+            var builder = new BackgroundTaskBuilder
+            {
+                Name = taskType.Name,
+                TaskEntryPoint = taskType.FullName
+            };
+            builder.SetTrigger(new TimeTrigger(20160, false)); // 14 days = 20160 minutes 
+            builder.AddCondition(new SystemCondition(SystemConditionType.InternetAvailable));
+            builder.Register();
+        }
+    }
+
+    /// <inheritdoc/>
     public void ToggleQuickResumeStartupTask(bool enable)
     {
         var taskType = typeof(StartupTask);
@@ -45,6 +68,7 @@ public class BackgroundTaskService : IBackgroundTaskService
         }
     }
 
+    /// <inheritdoc/>
     public void ToggleStreakReminderTask(bool enable)
     {
         var taskType = typeof(StreakReminderTask);
@@ -65,6 +89,7 @@ public class BackgroundTaskService : IBackgroundTaskService
         builder.Register();
     }
 
+    /// <inheritdoc/>
     public void UnregisterTask(string name)
     {
         foreach (var bgTask in BackgroundTaskRegistration.AllTasks)
