98cd45e37377d300013b6253e39974fa74826c48
Daniel Paulino <dpaulino@users.noreply.github.com>
2025-07-13 22:06:13 -0700

    Initial commit of relax v2 page

---
+++ src/AmbientSounds.Uwp/Controls/SleepTimerBar.xaml.cs
@@ -0,0 +1,21 @@
+﻿using AmbientSounds.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Windows.UI.Xaml.Controls;
+
+#nullable enable
+
+namespace AmbientSounds.Controls;
+public sealed partial class SleepTimerBar : UserControl
+{
+    public SleepTimerBar()
+    {
+        this.InitializeComponent();
+        this.DataContext = App.Services.GetRequiredService<SleepTimerViewModel>();
+    }
+
+    public SleepTimerViewModel ViewModel => (SleepTimerViewModel)this.DataContext;
+
+    public void Initialize() => ViewModel.Initialize();
+
+    public void Uninitialize() => ViewModel.Uninitialize();
+}
