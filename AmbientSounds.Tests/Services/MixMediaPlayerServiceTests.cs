ca869d30d5e96acda72fbc9c7013740f84014f38
Daniel Paulino <dpaulino@users.noreply.github.com>
2025-09-03 21:42:14 -0700

    Updated tests

--- AmbientSounds.Tests/Services/MixMediaPlayerServiceTests.cs
+++ AmbientSounds.Tests/Services/MixMediaPlayerServiceTests.cs
@@ -23,7 +23,6 @@ public class MixMediaPlayerServiceTests
         string testPath = "testPath";
         string testSoundId = "testSoundId";
         var mixMediaPlayerService = new MixMediaPlayerService(
-            Mock.Of<IUserSettings>(x => x.Get<int>(UserSettingsConstants.MaxActive) == 3),
             Mock.Of<ISoundService>(x => x.GetRandomSoundAsync() == Task.FromResult(new Sound { Id = testSoundId, FilePath = testPath })),
             Mock.Of<IAssetLocalizer>(),
             Mock.Of<IDispatcherQueue>(),
@@ -32,7 +31,8 @@ public class MixMediaPlayerServiceTests
                     x.SetSourceAsync(It.IsAny<string>(), It.IsAny<bool>()) == Task.FromResult(true))),
             Mock.Of<ISystemInfoProvider>(x => x.LocalFolderPath() == testPath),
             Mock.Of<ISystemMediaControls>(),
-            Mock.Of<ISoundVolumeService>())
+            Mock.Of<ISoundVolumeService>(),
+            Mock.Of<IIapService>(x => x.CanShowPremiumButtonsAsync() == Task.FromResult(true)))
         {
             GlobalVolume = 0.5
         };
