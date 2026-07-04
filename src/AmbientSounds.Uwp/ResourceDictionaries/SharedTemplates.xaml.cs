317d5279fe6a83e861f1047f68131a680b56f4c9
Daniel Paulino <dpaulino@users.noreply.github.com>
2026-07-04 08:59:44 -0700

    Replacing catalogue row control with asset row control

--- src/AmbientSounds.Uwp/ResourceDictionaries/SharedTemplates.xaml.cs
+++ src/AmbientSounds.Uwp/ResourceDictionaries/SharedTemplates.xaml.cs
@@ -2,6 +2,9 @@
 // in resource dictionaries.
 // Ref: https://docs.microsoft.com/en-us/windows/uwp/data-binding/data-binding-in-depth#resource-dictionaries-with-xbind
 
+using AmbientSounds.ViewModels;
+using Windows.UI.Xaml;
+
 namespace AmbientSounds.ResourceDictionaries;
 
 public partial class SharedTemplates
@@ -9,5 +12,13 @@ public partial class SharedTemplates
     public SharedTemplates()
     {
         InitializeComponent();
+    }
+
+    private void OnGridTapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+    {
+        if (sender is FrameworkElement g && g.DataContext is OnlineSoundViewModel vm)
+        {
+            _ = vm.PrimaryActionCommand.ExecuteAsync(null);
+        }
     }
 }
