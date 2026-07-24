c5b48fb3cfb210dcdb411eecd4015bf7f5794870
Daniel Paulino <dpaulino@users.noreply.github.com>
2026-07-23 23:26:10 -0700

    copy animation

--- src/AmbientSounds.Uwp/Views/ShellPage.xaml.cs
+++ src/AmbientSounds.Uwp/Views/ShellPage.xaml.cs
@@ -7,6 +7,7 @@ using JeniusApps.Common.Telemetry;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using Windows.Services.Store;
 using Windows.UI.ViewManagement;
@@ -24,6 +25,8 @@ namespace AmbientSounds.Views;
 /// </summary>
 public sealed partial class ShellPage : Page
 {
+    private readonly SemaphoreSlim _copyLock = new(1, 1);
+
     public ShellPage()
     {
         this.InitializeComponent();
@@ -132,5 +135,21 @@ public sealed partial class ShellPage : Page
             App.Services.GetRequiredService<ITelemetry>().TrackEvent(TelemetryConstants.ShellInAppShareOpened);
             FlyoutBase.ShowAttachedFlyout(fe);
         }
+    }
+
+    private async void InAppShareCopyClicked(object sender, RoutedEventArgs e)
+    {
+        if (_copyLock.CurrentCount == 0)
+        {
+            return;
+        }
+
+        await _copyLock.WaitAsync();
+        InAppShareCopySucceededIcon.Visibility = Visibility.Visible;
+        InAppShareCopyIcon.Visibility = Visibility.Collapsed;
+        await Task.Delay(3000);
+        InAppShareCopyIcon.Visibility = Visibility.Visible;
+        InAppShareCopySucceededIcon.Visibility = Visibility.Collapsed;
+        _copyLock.Release();
     }
 }
