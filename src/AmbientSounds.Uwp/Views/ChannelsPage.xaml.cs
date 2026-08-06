416cf87236b75a832e3ccd689ae74f3cd1965c46
Daniel Paulino <dpaulino@users.noreply.github.com>
2026-08-05 22:42:27 -0700

    Unpolished but working preview

--- src/AmbientSounds.Uwp/Views/ChannelsPage.xaml.cs
+++ src/AmbientSounds.Uwp/Views/ChannelsPage.xaml.cs
@@ -1,10 +1,14 @@
 ﻿using AmbientSounds.ViewModels;
 using JeniusApps.Common.Telemetry;
+using JeniusApps.Common.Tools;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Toolkit.Uwp.UI;
 using System;
 using System.ComponentModel;
 using System.Threading;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
@@ -28,7 +32,6 @@ public sealed partial class ChannelsPage : Page
     {
         App.Services.GetRequiredService<ITelemetry>().TrackPageView(nameof(ChannelsPage));
         ViewModel.PropertyChanged += OnPropertyChanged;
-        ViewModel.GridVideoPlayed += OnVideoPlayed;
 
         _cts ??= new();
 
@@ -47,7 +50,6 @@ public sealed partial class ChannelsPage : Page
     protected override void OnNavigatedFrom(NavigationEventArgs e)
     {
         ViewModel.PropertyChanged -= OnPropertyChanged;
-        ViewModel.GridVideoPlayed -= OnVideoPlayed;
         _cts?.Cancel();
         _cts = null;
         ViewModel.Uninitialize();
@@ -55,33 +57,53 @@ public sealed partial class ChannelsPage : Page
 
     private async void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
     {
-        if (e.PropertyName == nameof(ViewModel.SelectedChannel) && ViewModel.SelectedChannel is not null)
+        if (e.PropertyName == nameof(ViewModel.SelectedChannel) && ViewModel.SelectedChannel is { } channel)
         {
+            PreviewVideoPlayer.Visibility = Visibility.Collapsed;
+            PreviewButton.Visibility = Visibility.Collapsed;
             await ManualContentFadeIn.StartAsync();
+            TryPlayPreviewVideo("https://ambiecataloguestorage.blob.core.windows.net/videopreviews/hokkaidoCoast.mp4");
         }
     }
 
-    private void OnGridViewLoaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+    private void TryPlayPreviewVideo(string videoUrl)
     {
-        if (sender is GridView gridView)
+        PreviewVideoPlayer.MediaPlayer.Pause();
+        if (videoUrl is { Length: > 0 } && Uri.TryCreate(videoUrl, UriKind.Absolute, out Uri uri))
         {
-            ScrollViewer? s = gridView.FindDescendant<ScrollViewer>();
-            if (s is not null)
-            {
-                s.CanContentRenderOutsideBounds = true;
-            }
+            PreviewVideoPlayer.Source = MediaSource.CreateFromUri(uri);
+            PreviewVideoPlayer.MediaPlayer.MediaEnded -= OnMediaEnded;
+            PreviewVideoPlayer.MediaPlayer.MediaEnded += OnMediaEnded;
+            PreviewVideoPlayer.MediaPlayer.Play();
+            PreviewVideoPlayer.Visibility = Visibility.Visible;
         }
     }
 
-    private void OnVideoPlayed(object sender, ChannelViewModel vm)
+    private void OnMediaEnded(MediaPlayer sender, object args)
     {
-        //var animation = ChannelsGrid.PrepareConnectedAnimation("channelVideoClicked", vm, "ImageRect");
-        //animation.Configuration = new BasicConnectedAnimationConfiguration();
+        App.Services.GetRequiredService<IDispatcherQueue>().TryEnqueue(() =>
+        {
+            PreviewButton.Visibility = Visibility.Visible;
+        });
     }
 
-    private async void OnClosePaneClicked(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+    private async void OnClosePaneClicked(object sender, RoutedEventArgs e)
     {
+        PreviewVideoPlayer.MediaPlayer.Pause();
+        PreviewVideoPlayer.MediaPlayer.MediaEnded -= OnMediaEnded;
+
         await PaneFadeOut.StartAsync();
         ViewModel.CloseDetailsCommand.Execute(null);
+        PreviewVideoPlayer.Visibility = Visibility.Collapsed;
+        PreviewButton.Visibility = Visibility.Collapsed;
+    }
+
+    private void OnPreviewButtonClicked(object sender, RoutedEventArgs e)
+    {
+        if (PreviewVideoPlayer.MediaPlayer.PlaybackSession.PlaybackState is MediaPlaybackState.Paused)
+        {
+            PreviewButton.Visibility = Visibility.Collapsed;
+            PreviewVideoPlayer.MediaPlayer.Play();
+        }
     }
 }
